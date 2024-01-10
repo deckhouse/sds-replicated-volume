8c4bae37224a898fa652d0500b325dc831fff625
Vasily Oleynikov <vasily.oleynikov@flant.com>
2024-01-10 23:23:40 +0300

    [controller] Added necessary checks for zone specifications in DRBDStorageClass validation webhook based on different topology settings (#15)
    Signed-off-by: v.oleynikov <vasily.oleynikov@flant.com>

--- images/webhooks/src/validators/dscValidator.go
+++ images/webhooks/src/validators/dscValidator.go
@@ -9,6 +9,7 @@ import (
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/klog/v2"
 	"net/http"
@@ -25,11 +26,13 @@ type Metadata struct {
 }
 
 type dscSpec struct {
-	IsDefault       bool   `json:"isDefault"`
-	ReclaimPolicy   string `json:"reclaimPolicy"`
-	Replication     string `json:"replication"`
-	StoragePool     string `json:"storagePool"`
-	PreferablyLocal string `json:"PreferablyLocal"`
+	IsDefault       bool     `json:"isDefault"`
+	ReclaimPolicy   string   `json:"reclaimPolicy"`
+	Replication     string   `json:"replication"`
+	StoragePool     string   `json:"storagePool"`
+	PreferablyLocal string   `json:"PreferablyLocal"`
+	Topology        string   `json:"topology"`
+	Zones           []string `json:"zones"`
 }
 
 func DSCValidate(w http.ResponseWriter, r *http.Request) {
@@ -52,17 +55,77 @@ func DSCValidate(w http.ResponseWriter, r *http.Request) {
 	dscJson := dsc{}
 	json.Unmarshal(raw, &dscJson)
 
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		klog.Fatal(err.Error())
+	}
+
+	client, err := dynamic.NewForConfig(config)
+	if err != nil {
+		klog.Fatal(err)
+	}
+
+	staticClient, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		klog.Fatal(err)
+	}
+
+	var clusterZoneList []string
+
+	nodes, _ := staticClient.CoreV1().Nodes().List(context.TODO(), metav1.ListOptions{})
+	for _, node := range nodes.Items {
+		for label, value := range node.GetObjectMeta().GetLabels() {
+			if label == "topology.kubernetes.io/zone" {
+				clusterZoneList = append(clusterZoneList, value)
+			}
+		}
+	}
+
+	if dscJson.Spec.Topology == "TransZonal" {
+		if len(dscJson.Spec.Zones) == 0 {
+			arReview.Response.Allowed = false
+			arReview.Response.Result = &metav1.Status{
+				Message: fmt.Sprintf("You must set at least one zone."),
+			}
+			klog.Infof("No zones in DRBDStorageClass (%s)", string(raw))
+		}
+		if (dscJson.Spec.Replication == "Availability" || dscJson.Spec.Replication == "ConsistencyAndAvailability") && len(dscJson.Spec.Zones) != 3 {
+			arReview.Response.Allowed = false
+			arReview.Response.Result = &metav1.Status{
+				Message: fmt.Sprintf("With replication set to Availability or ConsistencyAndAvailability, three zones need to be specified."),
+			}
+			klog.Infof("Incorrect combination of replication and zones (%s)", string(raw))
+		}
+	}
+
+	if dscJson.Spec.Topology == "Zonal" {
+		if len(dscJson.Spec.Zones) != 0 {
+			arReview.Response.Allowed = false
+			arReview.Response.Result = &metav1.Status{
+				Message: fmt.Sprintf("No zones must be set with Zonal topology."),
+			}
+			klog.Infof("No zones must be set with Zonal topology (%s)", string(raw))
+		}
+	}
+
+	if dscJson.Spec.Topology == "Ignored" {
+		if len(clusterZoneList) != 0 {
+			arReview.Response.Allowed = false
+			arReview.Response.Result = &metav1.Status{
+				Message: fmt.Sprintf("In a cluster with existing zones, the Ignored topology should not be used."),
+			}
+			klog.Infof("In a cluster with existing zones, the Ignored topology should not be used (%s)", string(raw))
+		}
+		if len(dscJson.Spec.Zones) != 0 {
+			arReview.Response.Allowed = false
+			arReview.Response.Result = &metav1.Status{
+				Message: fmt.Sprintf("No zones must be set with Ignored topology."),
+			}
+			klog.Infof("No zones must be set with Ignored topology (%s)", string(raw))
+		}
+	}
+
 	if dscJson.Spec.IsDefault == true {
-		config, err := rest.InClusterConfig()
-		if err != nil {
-			klog.Fatal(err.Error())
-		}
-
-		client, err := dynamic.NewForConfig(config)
-		if err != nil {
-			klog.Fatal(err)
-		}
-
 		dscRes := schema.GroupVersionResource{Group: "storage.deckhouse.io", Version: "v1alpha1", Resource: "drbdstorageclasses"}
 
 		ctx := context.Background()
