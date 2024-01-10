a48f670ef5d0e71f1ea42c36abc230bbb6e37038
Alexandr Ohrimenko <alexandr.ohrimenko@flant.com>
2024-01-10 09:44:17 +0300

    license added
    Signed-off-by: Alexandr Ohrimenko <alexandr.ohrimenko@flant.com>

--- images/rnd-csi/driver/identity.go
+++ images/rnd-csi/driver/identity.go
@@ -1,6 +1,5 @@
 /*
-Copyright 2020 DigitalOcean
-Copyright 2020 Flant
+Copyright 2023 Flant JSC
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
