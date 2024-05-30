09e14cf13dd96a89e78704f27a8b3e60356c3d15
Nikolay Demchuk <nikolay.demchuk@flant.com>
2024-05-30 18:18:48 +1000

    Update client-wrapper.sh
    Signed-off-by: Nikolay Demchuk <nikolay.demchuk@flant.com>

--- images/linstor-server/client-wrapper.sh
+++ images/linstor-server/client-wrapper.sh
@@ -100,6 +100,10 @@ if [[ $(echo "${valid_subcommands_list[@]}" | fgrep -w -- $1) ]]; then
     allowed=true
   fi
 
+  if [[ "$2" == "activate" || "$2" == "deactivate" ]]; then
+    allowed=true
+  fi
+
   if [[ "$2" == "set-property" ]] && [[ "$4" == "AutoplaceTarget" ]]; then
     allowed=true
   fi
