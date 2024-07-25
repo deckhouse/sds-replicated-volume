4fcf3313c3e8675efc8b7b4fd1763b5ece39c2f4
Aleksandr Zimin <alexandr.zimin@flant.com>
2024-07-25 21:44:21 +0300

    fix webhook deletion
    Signed-off-by: Aleksandr Zimin <alexandr.zimin@flant.com>

--- hooks/migrate_csi_endpoint.sh
+++ hooks/migrate_csi_endpoint.sh
@@ -31,6 +31,7 @@ run_trigger() {
   export NAMESPACE="d8-sds-replicated-volume"
   export timestamp="$(date +"%Y%m%d%H%M%S")"
   export affected_pvs_hash=""
+  export WEBHOOK_NAME="d8-sds-replicated-volume-sc-validation"
   echo "Migration csi shell hook started"
 
   if kubectl get secret "$SECRET_NAME" -n "$NAMESPACE" > /dev/null 2>&1; then
@@ -64,8 +65,13 @@ run_trigger() {
   cd "$temp_dir"
   echo "temporary dir: $temp_dir"
 
-  echo "Deleting validatingwebhookconfiguration d8-sds-replicated-volume-sc-validation"
-  kubectl delete validatingwebhookconfigurations.admissionregistration.k8s.io d8-sds-replicated-volume-sc-validation
+  if kubectl get validatingwebhookconfigurations.admissionregistration.k8s.io "${WEBHOOK_NAME}" &>/dev/null; then
+    echo "Deleting validatingwebhookconfiguration ${WEBHOOK_NAME}"
+    kubectl delete validatingwebhookconfigurations.admissionregistration.k8s.io "${WEBHOOK_NAME}"
+  else
+    echo "ValidatingWebhookConfiguration ${WEBHOOK_NAME} does not exist. Skipping deletion"
+  fi
+
 
   migrate_storage_classes
   migrate_pvc_pv
