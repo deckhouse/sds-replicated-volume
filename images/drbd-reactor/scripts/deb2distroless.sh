4204ab9b086c3a629a380f012192efa1c94cf12e
Ivan.Makeev <ivan.makeev@flant.com>
2024-10-23 17:19:38 +0400

    [drbd-reactor] WIP
    Signed-off-by: Ivan.Makeev <ivan.makeev@flant.com>

--- images/drbd-reactor/scripts/deb2distroless.sh
+++ images/drbd-reactor/scripts/deb2distroless.sh
@@ -1,76 +1,129 @@
 #!/bin/bash
 
-# based on https://github.com/stickr-eng/heroku-buildpack-deb/blob/master/bin/compile
-DEB_FILE=$1
+# https://github.com/stickr-eng/heroku-buildpack-deb/blob/master/bin/compile
+# DEB_FILE=$1
 
 IAM=$(basename $0)
 EXCLUDED_DIRS=("etc/init.d" "lib/systemd" "usr/share/doc" "usr/share/man")
+BIN_DIRS=("usr/sbin" "usr/bin" "bin" "sbin")
 
 function log() {
 	local prefix="[$IAM]"
 	echo "${prefix} $@"
 }
 
-if [ -z "${DEB_FILE}" -o ! -r "${DEB_FILE}" ]; then
-	log "ERROR: Package ${DEB_FILE} not found!"
-  exit 2
+if ! [ -x "$(command -v apt-get)" ]; then
+  log "ERROR: apt-get is not installed." >&2
+  exit 1
 fi
 
-DEB_BASENAME=$(basename $DEB_FILE)
-# remove the last hyphen and everything after it
-PKG_NAME=${DEB_BASENAME%-*}
-
-EXTRACT_DIR="${2:-"/relocate/${PKG_NAME}"}"
-mkdir -p "${EXTRACT_DIR}"
-
-log "Installing $PKG_NAME from $DEB_FILE to ${EXTRACT_DIR}..."
-
-cd "${EXTRACT_DIR}"
-ar vx $DEB_FILE
-
-if [ -f "data.tar.xz" ]; then
-	log "xz archive detected"
-	tar -xJvf data.tar.xz
-	rm data.tar.xz
-elif [ -f "data.tar.zst" ]; then
-	log "zstd archive detected"
-	tar --zstd -xvf data.tar.zst
-	rm data.tar.zst
-elif [ -f "data.tar.gz" ]; then
-	log "gz archive detected"
-	tar -xzvf data.tar.gz
-	rm data.tar.gz
+if ! [ -x "$(command -v dpkg-deb)" ]; then
+  log "ERROR: dpkg-deb is not installed." >&2
+  exit 1
 fi
 
-# remove unnecessary files
-rm -f debian-binary control.tar.*
+if [ -z "$( ls -A '/var/lib/apt/lists' )" ]; then
+  log "ERROR: You must do 'apt-get update' before use this script."
+	exit 3
+fi
 
-# generate a find-command to exclude some directories
-# https://stackoverflow.com/a/16595367
-FIND_EXCLUDED_DIRS=()
+function download() {
+	local download_pkg=$1
+	local download_dir=$2
 
-len=${#EXCLUDED_DIRS[@]}
-cur=0
+	log "Downloading ${download_pkg} to ${download_dir}..."
+	mkdir -p $download_dir
 
-if [ $len -gt 0 ]; then
+	apt-get --download-only -o Dir::Cache="${download_dir}/" -o Debug::NoLocking=1 install ${download_pkg}
+}
+
+function extract() {
+	local deb_file=$1
+  local deb_basename=$(basename $deb_file)
+  # remove the last hyphen and everything after it
+  local pkg_name=${deb_basename%-*}
+
+	local extract_dir="${2:-"/relocate/${pkg_name}"}"
+	local download_dir=$(pwd)
+
+  if [ -z "${deb_file}" -o ! -r "${deb_file}" ]; then
+  	log "ERROR: Package ${deb_file} not found!"
+    exit 2
+  fi
+
+  mkdir -p "${extract_dir}"
+
+  log "Installing $pkg_name from $deb_file to ${extract_dir}..."
+
+  cd "${extract_dir}"
+  ar vx $deb_file
+
+  if [ -f "data.tar.xz" ]; then
+  	log "xz archive detected"
+  	tar -xJvf data.tar.xz
+  	rm data.tar.xz
+  elif [ -f "data.tar.zst" ]; then
+  	log "zstd archive detected"
+  	tar --zstd -xvf data.tar.zst
+  	rm data.tar.zst
+  elif [ -f "data.tar.gz" ]; then
+  	log "gz archive detected"
+  	tar -xzvf data.tar.gz
+  	rm data.tar.gz
+  fi
+
+  # remove unnecessary files
+  rm -f debian-binary control.tar.*
+
 	for dir in ${EXCLUDED_DIRS[@]}; do
-	  dir_name="${EXTRACT_DIR}/${dir}"
+	  dir_name="${extract_dir}/${dir}"
 		if [ -d "${dir_name}" ]; then
 	    log "Remove directory '${dir_name}' because its excluded"
 			rm -rf "${dir_name}"
 		fi
-		# cur=$((cur + 1))
-		# FIND_EXCLUDED_DIRS+=("-not" "\\(" "-path" "'./$dir'" "-prune" "\\)")
-		# if [[ "$cur" -lt "$len" ]]; then
-		#   # not a last element, add -or
-		# 	FIND_EXCLUDED_DIRS+=("-o")
-		# fi
 	done
-	# FIND_EXCLUDED_DIRS+=("-prune" "\\)")
-fi
-#find_cmd="find . ${FIND_EXCLUDED_DIRS[@]} -type f -print"
-#| xargs -0 -I {} -t cp -vf --parents {} /"
-# echo "find cmd: ${find_cmd}"
+
+	# apt-cache depends --recurse --no-recommends --no-suggests \
+	# --no-conflicts --no-breaks --no-replaces --no-enhances \
+	# --no-pre-depends ${f} | grep "^\w"
+
+	# IFS=$'\n' depends=( $(dpkg-deb -I $deb_file | grep -E "Depends|Recommends|Suggests|Pre\-Depends" | tr -d "|," | sed "s/([^)]*)/()/g" | tr -d "()" | tr " " "\n" | grep -Ev "Depends|Recommends|Suggests|Pre\-Depends") )
+	# for dep in "${depends[@]}"; do
+	# 	echo "dep: ${dep}"
+	# 	download "$dep" "${download_dir}"
+	# done
+}
+
+function extract_libraries() {
+	local deb_file=$1
+  local deb_basename=$(basename $deb_file)
+  # remove the last hyphen and everything after it
+  local pkg_name=${deb_basename%-*}
+
+	local extract_dir="${2:-"/relocate/${pkg_name}"}"
+
+	for dir in ${BIN_DIRS[@]}; do
+	  dir_name="${extract_dir}/${dir}"
+		if [ ! -d "${dir_name}" ]; then
+			continue
+		fi
+
+		# find only non-empty binary files (https://stackoverflow.com/a/47678132)
+#		find ${dir_name} -type f ! -size 0 -print0 -exec grep -IL . "{}" \; |
+    while IFS= read -r -d '' binfile; do
+      log "[extract_libraries] Process binary file: $binfile"
+    done < <(find ${dir_name} -type f ! -size 0 -print0 -exec grep -IL . "{}" \;)
+	done
+
+# ldd /bin/bash | awk '/=>/{print $(NF-1)}'  |
+#  while read n; do apt-file search $n; done |
+#   awk '{print $1}' | sed 's/://' | sort | uniq
+}
+
+extract $@
+extract_libraries $@
+
+# generate a find-command to exclude some directories
+# https://stackoverflow.com/a/16595367
+
 # find . -not \( -path ./lib/systemd -prune \) -not \( -path ./usr/share/doc -prune \) -type f -print
-# without eval I get error like find: paths must precede expression: `\(' :-(
-#eval ${find_cmd}
