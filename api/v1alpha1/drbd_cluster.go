0167a7984fbe9fb59b3cbc425c8d7f18d099b0bc
slava v <slavav@MacBook-Pro-slava.local>
2024-12-10 14:45:30 +0700

    Added drbd cluster and drbd node CRD spec, added both as Go structs
    Signed-off-by: slava v <vyacheslav.voytenok@flant.com>

---
+++ api/v1alpha1/drbd_cluster.go
@@ -0,0 +1,107 @@
+/*
+Copyright 2023 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// DRBDClusterSpec defines the desired state of DRBDCluster
+type DRBDClusterSpec struct {
+	Replicas                  int                        `json:"replicas"`
+	QuorumPolicy              string                     `json:"quorumPolicy"`
+	NetworkPoolName           string                     `json:"networkPoolName"`
+	SharedSecret              string                     `json:"sharedSecret"`
+	Size                      int64                      `json:"size"`
+	DrbdCurrentGi             string                     `json:"drbdCurrentGi"`
+	Port                      int                        `json:"port"`
+	Minor                     int                        `json:"minor"`
+	AttachmentRequested       []string                   `json:"attachmentRequested"`
+	TopologySpreadConstraints []TopologySpreadConstraint `json:"topologySpreadConstraints,omitempty"`
+	Affinity                  Affinity                  `json:"affinity,omitempty"`
+	AutoDiskful               AutoDiskful               `json:"autoDiskful,omitempty"`
+	AutoRecovery              AutoRecovery              `json:"autoRecovery,omitempty"`
+	StoragePoolSelector       []LabelSelector            `json:"storagePoolSelector,omitempty"`
+}
+
+// TopologySpreadConstraint specifies topology constraints
+type TopologySpreadConstraint struct {
+	MaxSkew           int    `json:"maxSkew"`
+	TopologyKey       string `json:"topologyKey"`
+	WhenUnsatisfiable string `json:"whenUnsatisfiable"`
+}
+
+// Affinity defines node affinity scheduling rules
+type Affinity struct {
+	NodeAffinity NodeAffinity `json:"nodeAffinity,omitempty"`
+}
+
+// NodeAffinity specifies node selection criteria
+type NodeAffinity struct {
+	RequiredDuringSchedulingIgnoredDuringExecution NodeSelector `json:"requiredDuringSchedulingIgnoredDuringExecution,omitempty"`
+}
+
+// NodeSelector represents constraints to match nodes
+type NodeSelector struct {
+	NodeSelectorTerms []NodeSelectorTerm `json:"nodeSelectorTerms"`
+}
+
+// NodeSelectorTerm defines node selection conditions
+type NodeSelectorTerm struct {
+	MatchExpressions []SelectorRequirement `json:"matchExpressions"`
+}
+
+// SelectorRequirement defines a key-value match condition
+type SelectorRequirement struct {
+	Key      string   `json:"key"`
+	Operator string   `json:"operator"`
+	Values   []string `json:"values"`
+}
+
+// AutoDiskful represents auto-diskful settings
+type AutoDiskful struct {
+	DelaySeconds int `json:"delaySeconds"`
+}
+
+// AutoRecovery represents auto-recovery settings
+type AutoRecovery struct {
+	DelaySeconds int `json:"delaySeconds"`
+}
+
+// LabelSelector defines match expressions for selecting resources
+type LabelSelector struct {
+	MatchExpressions []SelectorRequirement `json:"matchExpressions"`
+}
+
+// DRBDClusterStatus defines the observed state of DRBDCluster
+type DRBDClusterStatus struct {
+	Size                int64              `json:"size"`
+	AttachmentCompleted []string           `json:"attachmentCompleted"`
+	Conditions          []metav1.Condition `json:"conditions"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// DRBDCluster is the Schema for the drbdclusters API
+type DRBDCluster struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   DRBDClusterSpec   `json:"spec"`
+	Status DRBDClusterStatus `json:"status,omitempty"`
+}
