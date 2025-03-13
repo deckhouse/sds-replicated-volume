7513ec2c6b6fbf850df731cbebb696f72ac07a60
Slava V <vyacheslav.voytenok@flant.com>
2025-03-13 21:52:58 +0700

    more enumer fix
    Signed-off-by: Slava V <vyacheslav.voytenok@flant.com>

--- images/linstor-csi/src/pkg/volume/paramkey_enumer.go
+++ images/linstor-csi/src/pkg/volume/paramkey_enumer.go
@@ -8,7 +8,7 @@ import (
 
 const _paramKeyName = "allowremotevolumeaccessautoplaceclientlistdisklessonremainingdisklessstoragepooldonotplacewithregexencryptionfsoptslayerlistmountoptsnodelistplacementcountplacementpolicyreplicasondifferentreplicasonsamesizekibstoragepoolpostmountxfsoptsresourcegroupusepvcnameoverprovisionstorageclassname"
 
-var _paramKeyIndex = [...]uint16{0, 23, 32, 42, 61, 80, 99, 109, 115, 124, 133, 141, 155, 170, 189, 203, 210, 221, 237, 250, 260, 273, 288}
+var _paramKeyIndex = [...]uint16{0, 23, 32, 42, 61, 80, 99, 109, 115, 124, 133, 141, 155, 170, 189, 203, 210, 221, 237, 250, 260, 273, 289}
 
 func (i paramKey) String() string {
 	if i < 0 || i >= paramKey(len(_paramKeyIndex)-1) {
@@ -41,7 +41,7 @@ var _paramKeyNameToValueMap = map[string]paramKey{
 	_paramKeyName[237:250]: 18,
 	_paramKeyName[250:260]: 19,
 	_paramKeyName[260:273]: 20,
-	_paramKeyName[273:288]: 21,
+	_paramKeyName[273:289]: 21,
 }
 
 // paramKeyString retrieves an enum value from the enum constants string name.
