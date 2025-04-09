7b56051053e82d412ba91c14b946369bd5a5c4c0
v.oleynikov <vasily.oleynikov@flant.com>
2025-04-09 13:22:06 +0300

    [internal] fixes in CI, switch to werf v2
    Signed-off-by: v.oleynikov <vasily.oleynikov@flant.com>

---
+++ images/sds-replicated-volume-controller/cmd/main.go
@@ -0,0 +1,179 @@
+/*
+Copyright 2025 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	goruntime "runtime"
+
+	lapi "github.com/LINBIT/golinstor/client"
+	snc "github.com/deckhouse/sds-node-configurator/api/v1alpha1"
+	"github.com/deckhouse/sds-replicated-volume/api/linstor"
+	srv "github.com/deckhouse/sds-replicated-volume/api/v1alpha1"
+	v1 "k8s.io/api/core/v1"
+	sv1 "k8s.io/api/storage/v1"
+	extv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	apiruntime "k8s.io/apimachinery/pkg/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	controllerruntime "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/cache"
+	"sigs.k8s.io/controller-runtime/pkg/healthz"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+
+	"github.com/deckhouse/sds-replicated-volume/images/sds-replicated-volume-controller/config"
+	"github.com/deckhouse/sds-replicated-volume/images/sds-replicated-volume-controller/pkg/controller"
+	kubutils "github.com/deckhouse/sds-replicated-volume/images/sds-replicated-volume-controller/pkg/kubeutils"
+	"github.com/deckhouse/sds-replicated-volume/images/sds-replicated-volume-controller/pkg/logger"
+)
+
+var (
+	resourcesSchemeFuncs = []func(*apiruntime.Scheme) error{
+		srv.AddToScheme,
+		snc.AddToScheme,
+		linstor.AddToScheme,
+		clientgoscheme.AddToScheme,
+		extv1.AddToScheme,
+		v1.AddToScheme,
+		sv1.AddToScheme,
+	}
+)
+
+func main() {
+	ctx := context.Background()
+
+	cfgParams, err := config.NewConfig()
+	if err != nil {
+		fmt.Println("unable to create NewConfig " + err.Error())
+	}
+
+	log, err := logger.NewLogger(cfgParams.Loglevel)
+	if err != nil {
+		fmt.Printf("unable to create NewLogger, err: %v\n", err)
+		os.Exit(1)
+	}
+
+	log.Info(fmt.Sprintf("Go Version:%s ", goruntime.Version()))
+	log.Info(fmt.Sprintf("OS/Arch:Go OS/Arch:%s/%s ", goruntime.GOOS, goruntime.GOARCH))
+
+	// Create default config Kubernetes client
+	kConfig, err := kubutils.KubernetesDefaultConfigCreate()
+	if err != nil {
+		log.Error(err, "error by reading a kubernetes configuration")
+	}
+	log.Info("read Kubernetes config")
+
+	// Setup scheme for all resources
+	scheme := apiruntime.NewScheme()
+	for _, f := range resourcesSchemeFuncs {
+		err := f(scheme)
+		if err != nil {
+			log.Error(err, "failed to add to scheme")
+			os.Exit(1)
+		}
+	}
+	log.Info("read scheme CR")
+
+	cacheOpt := cache.Options{
+		DefaultNamespaces: map[string]cache.Config{
+			cfgParams.ControllerNamespace: {},
+		},
+	}
+
+	managerOpts := manager.Options{
+		Scheme: scheme,
+		// MetricsBindAddress: cfgParams.MetricsPort,
+		HealthProbeBindAddress:  cfgParams.HealthProbeBindAddress,
+		Cache:                   cacheOpt,
+		LeaderElection:          true,
+		LeaderElectionNamespace: cfgParams.ControllerNamespace,
+		LeaderElectionID:        config.ControllerName,
+	}
+
+	mgr, err := manager.New(kConfig, managerOpts)
+	if err != nil {
+		log.Error(err, "failed to create a manager")
+		os.Exit(1)
+	}
+	log.Info("created kubernetes manager in namespace: " + cfgParams.ControllerNamespace)
+
+	controllerruntime.SetLogger(log.GetLogger())
+	lc, err := lapi.NewClient(lapi.Log(log))
+	if err != nil {
+		log.Error(err, "failed to create a linstor client")
+		os.Exit(1)
+	}
+
+	if _, err := controller.NewLinstorNode(mgr, lc, cfgParams.ConfigSecretName, cfgParams.ScanInterval, *log); err != nil {
+		log.Error(err, "failed to create the NewLinstorNode controller")
+		os.Exit(1)
+	}
+	log.Info("the NewLinstorNode controller starts")
+
+	if _, err := controller.NewReplicatedStorageClass(mgr, cfgParams, *log); err != nil {
+		log.Error(err, "failed to create the NewReplicatedStorageClass controller")
+		os.Exit(1)
+	}
+	log.Info("the NewReplicatedStorageClass controller starts")
+
+	if _, err := controller.NewReplicatedStoragePool(mgr, lc, cfgParams.ScanInterval, *log); err != nil {
+		log.Error(err, "failed to create the NewReplicatedStoragePool controller")
+		os.Exit(1)
+	}
+	log.Info("the NewReplicatedStoragePool controller starts")
+
+	if err = controller.NewLinstorPortRangeWatcher(mgr, lc, cfgParams.ScanInterval, *log); err != nil {
+		log.Error(err, "failed to create the NewLinstorPortRangeWatcher controller")
+		os.Exit(1)
+	}
+	log.Info("the NewLinstorPortRangeWatcher controller starts")
+
+	if err = controller.NewLinstorLeader(mgr, cfgParams.LinstorLeaseName, cfgParams.ScanInterval, *log); err != nil {
+		log.Error(err, "failed to create the NewLinstorLeader controller")
+		os.Exit(1)
+	}
+	log.Info("the NewLinstorLeader controller starts")
+
+	if err = controller.NewStorageClassAnnotationsReconciler(mgr, cfgParams.ScanInterval, *log); err != nil {
+		log.Error(err, "failed to create the NewStorageClassAnnotationsReconciler controller")
+		os.Exit(1)
+	}
+
+	controller.NewLinstorResourcesWatcher(mgr, lc, cfgParams.LinstorResourcesReconcileInterval, *log)
+	log.Info("the NewLinstorResourcesWatcher controller starts")
+
+	controller.RunReplicatedStorageClassWatcher(mgr, lc, cfgParams.ReplicatedStorageClassWatchInterval, *log)
+	log.Info("the RunReplicatedStorageClassWatcher controller starts")
+
+	if err = mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
+		log.Error(err, "unable to set up health check")
+		os.Exit(1)
+	}
+	if err = mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
+		log.Error(err, "unable to set up ready check")
+		os.Exit(1)
+	}
+
+	err = mgr.Start(ctx)
+	if err != nil {
+		log.Error(err, "error by starting the manager")
+		os.Exit(1)
+	}
+
+	log.Info("starting the manager")
+}
---
+++ images/webhooks/handlers/func.go
@@ -0,0 +1,107 @@
+/*
+Copyright 2025 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package handlers
+
+import (
+	"context"
+	"net/http"
+	"os"
+
+	snc "github.com/deckhouse/sds-node-configurator/api/v1alpha1"
+	"github.com/go-logr/logr"
+	kwhhttp "github.com/slok/kubewebhook/v2/pkg/http"
+	"github.com/slok/kubewebhook/v2/pkg/log"
+	"github.com/slok/kubewebhook/v2/pkg/model"
+	kwhvalidating "github.com/slok/kubewebhook/v2/pkg/webhook/validating"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/api/resource/v1alpha3"
+	sv1 "k8s.io/api/storage/v1"
+	extv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	apiruntime "k8s.io/apimachinery/pkg/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	controllerruntime "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	ctrllog "sigs.k8s.io/controller-runtime/pkg/log"
+
+	mc "github.com/sds-replicated-volume/images/webhooks/api"
+)
+
+func NewKubeClient(kubeconfigPath string) (client.Client, error) {
+	var config *rest.Config
+	var err error
+
+	if kubeconfigPath == "" {
+		kubeconfigPath = os.Getenv("kubeconfig")
+	}
+
+	controllerruntime.SetLogger(logr.New(ctrllog.NullLogSink{}))
+
+	config, err = clientcmd.BuildConfigFromFlags("", kubeconfigPath)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var (
+		resourcesSchemeFuncs = []func(*apiruntime.Scheme) error{
+			v1alpha3.AddToScheme,
+			mc.AddToScheme,
+			snc.AddToScheme,
+			clientgoscheme.AddToScheme,
+			extv1.AddToScheme,
+			v1.AddToScheme,
+			sv1.AddToScheme,
+		}
+	)
+
+	scheme := apiruntime.NewScheme()
+	for _, f := range resourcesSchemeFuncs {
+		err = f(scheme)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	clientOpts := client.Options{
+		Scheme: scheme,
+	}
+
+	return client.New(config, clientOpts)
+}
+
+func GetValidatingWebhookHandler(validationFunc func(ctx context.Context, _ *model.AdmissionReview, obj metav1.Object) (*kwhvalidating.ValidatorResult, error), validatorID string, obj metav1.Object, logger log.Logger) (http.Handler, error) {
+	validatorFunc := kwhvalidating.ValidatorFunc(validationFunc)
+
+	validatingWebhookConfig := kwhvalidating.WebhookConfig{
+		ID:        validatorID,
+		Obj:       obj,
+		Validator: validatorFunc,
+		Logger:    logger,
+	}
+
+	mutationWebhook, err := kwhvalidating.NewWebhook(validatingWebhookConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	mutationWebhookHandler, err := kwhhttp.HandlerFor(kwhhttp.HandlerConfig{Webhook: mutationWebhook, Logger: logger})
+
+	return mutationWebhookHandler, err
+}
