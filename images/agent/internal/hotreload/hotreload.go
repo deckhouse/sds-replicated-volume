037aaee4c0473ed13da0b41d845c1bbf46e64690
Aleksandr Stefurishin <aleksandr.stefurishin@flant.com>
2025-07-09 19:11:27 +0300

    hotreload
    Signed-off-by: Aleksandr Stefurishin <aleksandr.stefurishin@flant.com>

--- images/agent/internal/hotreload/hotreload.go
+++ images/agent/internal/hotreload/hotreload.go
@@ -2,18 +2,26 @@ package hotreload
 
 import (
 	"context"
+	"errors"
 	"log/slog"
 	"os"
 	"os/exec"
+	"sync"
+	"syscall"
 	"time"
 )
+
+const HotReloadEnabledEnvVar = "HOT_RELOAD_ENABLED"
+
+type Option func(*Options)
 
 type Options struct {
 	// Only parent process will use this logger. Default is [slog.Default].
 	Logger *slog.Logger
 
-	// Will be used to wait for reload. First call should return as soon as
-	// child process is ready to be spawned.
+	// Will be used to wait for reload. Should block until either reload or
+	// context cancelation. First call should return as soon as child process is
+	// ready to be spawned.
 	//
 	// Default is [WithPeriodicalModtimeChecker(os.Args[0], time.Second)]
 	WaitForChanges func(ctx context.Context)
@@ -21,12 +29,30 @@ type Options struct {
 	// Will be used to create a child command. By default, current process will
 	// be launched with the same arguments and environment variables, with
 	// stdout and stderr forwarding.
-	CreateCommand func() *exec.Cmd
+	// Command should consider ctx, i.e. spawned process should be killed on
+	// cancelation.
+	CreateCommand func(ctx context.Context) *exec.Cmd
 }
 
-const HotReloadEnabledEnvVar = "HOT_RELOAD_ENABLED"
+func newOptions(opts ...Option) *Options {
+	// defaults
+	o := &Options{}
+	WithLogger(slog.Default())(o)
+	WithPeriodicalModtimeChecker(os.Args[0], time.Second)(o)
+	WithCommand(func(ctx context.Context) *exec.Cmd {
+		cmd := exec.CommandContext(ctx, os.Args[0], os.Args[1:]...)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		cmd.Env = os.Environ()
+		return cmd
+	})(o)
 
-type Option func(*Options)
+	// overrides
+	for _, fn := range opts {
+		fn(o)
+	}
+	return o
+}
 
 func WithLogger(logger *slog.Logger) Option {
 	return func(o *Options) {
@@ -70,7 +96,7 @@ func WithPeriodicalModtimeChecker(filename string, period time.Duration) Option 
 	}
 }
 
-func WithCommand(createCommand func() *exec.Cmd) Option {
+func WithCommand(createCommand func(ctx context.Context) *exec.Cmd) Option {
 	return func(o *Options) {
 		o.CreateCommand = createCommand
 	}
@@ -87,78 +113,129 @@ func WithCommand(createCommand func() *exec.Cmd) Option {
 // second for reloads.
 func Enable(ctx context.Context, opts ...Option) {
 	// child process returns immediately
-	if os.Getenv(HotReloadEnabledEnvVar) == "1" {
+	if os.Getenv(HotReloadEnabledEnvVar) != "1" {
 		return
 	}
 
-	o := &Options{}
+	o := newOptions(opts...)
 
-	// defaults
-	WithLogger(slog.Default())
-	WithPeriodicalModtimeChecker(os.Args[0], time.Second)
-	WithCommand(func() *exec.Cmd {
-		cmd := exec.Command(os.Args[0], os.Args[1:]...)
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		cmd.Env = os.Environ()
-		return cmd
-	})
-
-	// overrides
-	for _, fn := range opts {
-		fn(o)
-	}
-
-	var cmd *exec.Cmd
-
-	killChild := func() *os.ProcessState {
-		err := cmd.Process.Kill()
-		if err != nil {
-			o.Logger.Error("error during Kill of child process", "err", err)
-		}
-
-		state, err := cmd.Process.Wait()
-		if err != nil {
-			o.Logger.Error("error during Wait of child process", "err", err)
-		}
-		return state
+	// initial wait to start
+	o.WaitForChanges(ctx)
+	if ctx.Err() != nil {
+		o.Logger.Info("exiting parent process before child process started")
+		os.Exit(0)
 	}
 
 	for {
-		o.WaitForChanges(ctx)
 		if ctx.Err() != nil {
-			if cmd == nil || cmd.Process == nil {
-				o.Logger.Info("exiting parent process before child process started")
-				os.Exit(0)
+			o.Logger.Info("not starting reloading, because parent process is exiting")
+			os.Exit(0)
+		}
+
+		childCtx, cancelChild := context.WithCancel(ctx)
+
+		wg := &sync.WaitGroup{}
+		wg.Add(1)
+
+		beforeRetry := func() {
+			cancelChild()
+			wg.Wait()
+			o.Logger.Info("retrying child process start in 1s...")
+			time.Sleep(time.Second)
+		}
+
+		// detect changes in background
+		var changeDetected bool
+		go func() {
+			defer wg.Done()
+
+			o.WaitForChanges(childCtx)
+
+			if ctx.Err() != nil {
+				o.Logger.Info("stopped waiting for changes, because parent process is exiting")
+				return
 			}
 
-			state := killChild()
+			if childCtx.Err() != nil {
+				o.Logger.Info("stopped waiting for changes, because child process exited")
+				return
+			}
 
-			o.Logger.Info(
-				"exiting parent process",
-				"exitCode", state.ExitCode(),
-				"ctxErr", ctx.Err(),
-			)
-			os.Exit(state.ExitCode())
-		}
+			// change detected
+			o.Logger.Info("change detected, reloading child process")
+			changeDetected = true
+			cancelChild()
+		}()
 
-		if cmd != nil {
-			// terminate old child process
-			state := killChild()
-			o.Logger.Info(
-				"old child process killed",
-				"exitCode", state.ExitCode(),
-			)
-		}
-
-		// spawn new child process
-		cmd = o.CreateCommand()
+		// start child process
+		cmd := o.CreateCommand(childCtx)
 
 		// prevent child process from forking itself
 		cmd.Env = append(cmd.Env, HotReloadEnabledEnvVar+"=0")
 
+		o.Logger.Info("starting child process", "cmd", cmd.String())
+
 		if err := cmd.Start(); err != nil {
 			o.Logger.Error("error during Start of child process", "err", err)
+			// retry
+			beforeRetry()
+			continue
+		}
+		o.Logger.Info("child process started", "pid", cmd.Process.Pid)
+
+		var childExitErr *exec.ExitError
+		if err := cmd.Wait(); err != nil {
+			if exitErr, ok := err.(*exec.ExitError); ok {
+				childExitErr = exitErr
+				o.Logger.Info("child process exited/killed", "exitCode", exitErr.ExitCode())
+			} else if !errors.Is(err, context.Canceled) {
+				o.Logger.Error("error during Wait of child process", "err", err)
+				// retry
+				beforeRetry()
+				continue
+			} else {
+				o.Logger.Info("child process canceled")
+			}
+		} else {
+			o.Logger.Info("child process exited successfully")
+		}
+
+		cancelChild()
+		wg.Wait()
+
+		var exiting bool
+
+		if ctx.Err() != nil {
+			o.Logger.Info("stopped reloading, because parent process is exiting")
+			exiting = true
+
+		} else if !changeDetected {
+			o.Logger.Info("stopped reloading, because child process exited/killed")
+			exiting = true
+		}
+
+		if exiting {
+			if childExitErr == nil || childExitErr.Exited() {
+				var code int
+				if childExitErr != nil {
+					code = childExitErr.ExitCode()
+				}
+				o.Logger.Info("exiting with child exit code", "code", code)
+				os.Exit(code)
+			}
+
+			var signal int
+			if ws, ok := childExitErr.Sys().(syscall.WaitStatus); ok {
+				signal = int(ws.Signal())
+			} else {
+				// problematic case (not on Unix?)
+			}
+
+			o.Logger.Warn("child process was killed with signal", "signal", signal)
+
+			// mimic typical shell behavior, when child process dies from a
+			// a signal
+			os.Exit(128 + signal)
 		}
 	}
 }
