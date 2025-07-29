a90b1d51858627fd99841d6b5f8d45daecb200cf
Pavel Karpov <pavel.karpov@flant.com>
2025-07-29 15:44:17 +0300

    add local_build.sh
    Signed-off-by: Pavel Karpov <pavel.karpov@flant.com>

---
+++ hack/local_build.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+
+cd ..
+
+# PAT TOKEN must be created here https://fox.flant.com/-/user_settings/personal_access_tokens
+# It must have api, read_api, read_repository, read_registry, write_registry scopes
+#PAT_TOKEN='REPLACEME'
+# prefix of the custom tag
+# the final image will look like: ${REGISTRY_PATH}:${CUSTOM_TAG}-<image_name>
+#CUSTOM_TAG=username
+REGISTRY_PATH=registry.flant.com/deckhouse/storage/localbuild
+if [ -z "$PAT_TOKEN" ];then
+  echo "ERR: empty PAT_TOKEN"
+  exit 1
+fi
+if [ -z "$CUSTOM_TAG" ];then
+  echo "ERR: empty CUSTOM_TAG"
+  exit 1
+fi
+
+# CI and werf variables
+export SOURCE_REPO="https://github.com"
+export CI_COMMIT_REF_NAME="null"
+
+print_help() {
+  echo "  Usage: $0 build|build_dev [<image_name1> [<image_name2> ...]]"
+  echo "  Possible actions: build, build_dev, enable_deckhouse, disable_deckhouse, werf_install"
+}
+
+build_action() {
+  #{ which werf | grep -qsE "^${HOME}/.trdl/"; } && werf_install
+
+  echo "Get base_images.yml"
+  _base_images get
+
+  echo "Start building for images:"
+  if [ -z "$IMAGE_NAMES" ]; then
+    echo "ERR: <no image names provided>"
+  else
+    werf cr login $REGISTRY_PATH --username='pat' --password=$PAT_TOKEN
+    for image in $IMAGE_NAMES; do
+      echo "Building image: $image"
+      werf build $image --add-custom-tag=$CUSTOM_TAG"-"$image --repo=$REGISTRY_PATH $1
+    done
+  fi
+
+  echo "Delete base_images.yml"
+  _base_images delete
+}
+
+build_dev_action() {
+  build_action --dev
+}
+
+disable_deckhouse() {
+  kubectl -n d8-system scale deployment/deckhouse --replicas 0
+}
+
+enable_deckhouse() {
+  kubectl -n d8-system scale deployment/deckhouse --replicas 1
+}
+
+werf_install() {
+  curl -sSL https://werf.io/install.sh | bash -s -- --version 2 --channel stable
+}
+
+_base_images() {
+  local ACTION=$1
+  if [ "$ACTION" = 'get' ];then
+    BASE_IMAGES_VERSION=$(grep -roP 'BASE_IMAGES_VERSION:\s+"v\d+\.\d+\.\d+"' | grep -oP 'v\d+\.\d+\.\d+' | head -n1)
+    if [ -z "$BASE_IMAGES_VERSION" ];then
+      echo "ERR: empty BASE_IMAGES_VERSION"
+      exit 1
+    fi
+    echo BASE_IMAGES_VERSION=$BASE_IMAGES_VERSION
+    curl -OJL https://fox.flant.com/api/v4/projects/deckhouse%2Fbase-images/packages/generic/base_images/${BASE_IMAGES_VERSION}/base_images.yml
+  else
+    rm -rf base_images.yml
+  fi
+}
+
+if [ $# -lt 1 ]; then
+  print_help
+  exit 1
+fi
+ACTION=$1
+
+shift
+
+if [ $# -eq 0 ]; then
+  IMAGE_NAMES=""
+else
+  IMAGE_NAMES="$@"
+fi
+
+case "$ACTION" in
+  build)
+    build_action
+    ;;
+  build_dev)
+    build_dev_action
+    ;;
+  enable_deckhouse)
+    enable_deckhouse
+    ;;
+  disable_deckhouse)
+    disable_deckhouse
+    ;;
+  werf_install)
+    werf_install
+    ;;
+  *)
+    echo "Unknown action: $ACTION"
+    print_help
+    exit 1
+    ;;
+esac
