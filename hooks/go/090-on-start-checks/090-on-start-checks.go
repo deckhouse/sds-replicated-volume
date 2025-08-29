7b7516eda616f523491ff42c70787747bb13035e
Nikolay Demchuk <nikolay.demchuk@flant.com>
2025-08-29 17:58:48 +1000

    fix Signed-off-by: Nikolay Demchuk <nikolay.demchuk@flant.com>

--- hooks/go/090-on-start-checks/090-on-start-checks.go
+++ hooks/go/090-on-start-checks/090-on-start-checks.go
@@ -35,38 +35,162 @@ var _ = registry.RegisterFunc(
 	onStartChecks,
 )
 
+// func onStartChecks(ctx context.Context, input *pkg.HookInput) error {
+// 	cl := input.DC.MustGetK8sClient()
+// 	logger := input.Logger
+
+// 	const (
+// 		crdGroup   = "internal.linstor.linbit.com"
+// 		crdVersion = "v1-15-0"
+// 		crdKind    = "PropsContainerList"
+// 		moduleName = "sds-replicated-volume"
+// 	)
+
+// 	propsList := &unstructured.UnstructuredList{}
+// 	propsList.SetGroupVersionKind(schema.GroupVersionKind{
+// 		Group:   crdGroup,
+// 		Version: crdVersion,
+// 		Kind:    crdKind,
+// 	})
+
+// 	if err := cl.List(ctx, propsList); err != nil {
+// 		return fmt.Errorf("failed to list propscontainers: %w", err)
+// 	}
+
+// 	logger.Info("processing propscontainers", "count", len(propsList.Items))
+
+// 	thinPoolExistence := false
+// 	patchedCount := 0
+
+// 	// Process each propscontainer
+// 	for _, item := range propsList.Items {
+// 		spec, found, _ := unstructured.NestedMap(item.Object, "spec")
+// 		if !found {
+// 			logger.Debug("propscontainer missing spec", "name", item.GetName())
+// 			continue
+// 		}
+
+// 		propKey, _, _ := unstructured.NestedString(spec, "prop_key")
+// 		propValue, _, _ := unstructured.NestedString(spec, "prop_value")
+
+// 		// Handle AutoEvictAllowEviction
+// 		if propKey == "DrbdOptions/AutoEvictAllowEviction" && propValue == "True" {
+// 			if err := patchPropsContainer(ctx, cl, &item, "False"); err != nil {
+// 				logger.Info("failed to patch propscontainer", "name", item.GetName(), "err", err)
+// 			} else {
+// 				logger.Info("patched AutoEvictAllowEviction", "name", item.GetName(), "value", "False")
+// 				patchedCount++
+// 			}
+// 		}
+
+// 		// Check for thin pool granularity
+// 		if propKey == "StorDriver/internal/lvmthin/thinPoolGranularity" {
+// 			thinPoolExistence = true
+// 			logger.Debug("found thin pool granularity setting", "name", item.GetName())
+// 		}
+// 	}
+
+// 	logger.Info("propscontainer processing complete", "total", len(propsList.Items), "patched", patchedCount)
+
+// 	// Handle thin provisioning setting
+// 	if thinPoolExistence {
+// 		if err := enableThinProvisioning(ctx, cl, moduleName); err != nil {
+// 			logger.Info("failed to enable thin provisioning", "err", err)
+// 		} else {
+// 			logger.Info("enabled thin provisioning", "module", moduleName)
+// 		}
+// 	} else {
+// 		logger.Debug("thin pool granularity not found, skipping thin provisioning enablement")
+// 	}
+
+// 	return nil
+// }
+
+// // patchPropsContainer patches a propscontainer with new prop_value
+// func patchPropsContainer(ctx context.Context, cl client.Client, item *unstructured.Unstructured, newValue string) error {
+// 	patch := map[string]interface{}{
+// 		"spec": map[string]interface{}{
+// 			"prop_value": newValue,
+// 		},
+// 	}
+
+// 	patchObj := &unstructured.Unstructured{Object: patch}
+// 	patchObj.SetGroupVersionKind(item.GroupVersionKind())
+// 	patchObj.SetName(item.GetName())
+
+// 	return cl.Patch(ctx, patchObj, client.MergeFrom(item))
+// }
+
+// // enableThinProvisioning enables thin provisioning in module config
+// func enableThinProvisioning(ctx context.Context, cl client.Client, moduleName string) error {
+// 	// Get existing module config
+// 	modCfg := &unstructured.Unstructured{}
+// 	modCfg.SetGroupVersionKind(schema.GroupVersionKind{
+// 		Group:   "deckhouse.io",
+// 		Version: "v1alpha1",
+// 		Kind:    "ModuleConfig",
+// 	})
+// 	modCfg.SetName(moduleName)
+
+// 	// Try to get existing config first
+// 	if err := cl.Get(ctx, client.ObjectKey{Name: moduleName}, modCfg); err != nil {
+// 		// If not found, create new one
+// 		patch := map[string]interface{}{
+// 			"spec": map[string]interface{}{
+// 				"settings": map[string]interface{}{
+// 					"enableThinProvisioning": true,
+// 				},
+// 			},
+// 		}
+// 		modCfg.SetUnstructuredContent(patch)
+// 		return cl.Create(ctx, modCfg)
+// 	}
+
+// 	// Update existing config
+// 	patch := map[string]interface{}{
+// 		"spec": map[string]interface{}{
+// 			"settings": map[string]interface{}{
+// 				"enableThinProvisioning": true,
+// 			},
+// 		},
+// 	}
+
+// 	patchObj := &unstructured.Unstructured{Object: patch}
+// 	patchObj.SetGroupVersionKind(modCfg.GroupVersionKind())
+// 	patchObj.SetName(moduleName)
+
+// 	return cl.Patch(ctx, patchObj, client.MergeFrom(modCfg))
+// }
+
+var _ = registry.RegisterFunc(
+	&pkg.HookConfig{
+		OnAfterHelm: &pkg.OrderedConfig{Order: 10},
+	},
+	onStartChecks,
+)
+
 func onStartChecks(ctx context.Context, input *pkg.HookInput) error {
 	cl := input.DC.MustGetK8sClient()
-	logger := input.Logger
 
-	const (
-		crdGroup   = "internal.linstor.linbit.com"
-		crdVersion = "v1-15-0"
-		crdKind    = "PropsContainerList"
-		moduleName = "sds-replicated-volume"
-	)
+	crdGroup := "internal.linstor.linbit.com"
+	crdVersion := "v1-15-0"
 
+	// List propscontainers
 	propsList := &unstructured.UnstructuredList{}
 	propsList.SetGroupVersionKind(schema.GroupVersionKind{
 		Group:   crdGroup,
 		Version: crdVersion,
-		Kind:    crdKind,
+		Kind:    "PropsContainerList",
 	})
 
 	if err := cl.List(ctx, propsList); err != nil {
-		return fmt.Errorf("failed to list propscontainers: %w", err)
+		return fmt.Errorf("listing propscontainers: %w", err)
 	}
 
-	logger.Info("processing propscontainers", "count", len(propsList.Items))
-
 	thinPoolExistence := false
-	patchedCount := 0
-
-	// Process each propscontainer
 	for _, item := range propsList.Items {
 		spec, found, _ := unstructured.NestedMap(item.Object, "spec")
 		if !found {
-			logger.Debug("propscontainer missing spec", "name", item.GetName())
 			continue
 		}
 
@@ -75,66 +199,30 @@ func onStartChecks(ctx context.Context, input *pkg.HookInput) error {
 
 		// Handle AutoEvictAllowEviction
 		if propKey == "DrbdOptions/AutoEvictAllowEviction" && propValue == "True" {
-			if err := patchPropsContainer(ctx, cl, &item, "False"); err != nil {
-				logger.Info("failed to patch propscontainer", "name", item.GetName(), "err", err)
+			patch := map[string]interface{}{
+				"spec": map[string]interface{}{
+					"prop_value": "False",
+				},
+			}
+			patchObj := &unstructured.Unstructured{Object: patch}
+			patchObj.SetGroupVersionKind(item.GroupVersionKind())
+			patchObj.SetName(item.GetName())
+
+			if err := cl.Patch(ctx, patchObj, client.MergeFrom(&item)); err != nil {
+				input.Logger.Info("Failed to patch propscontainer", "name", item.GetName(), "err", err)
 			} else {
-				logger.Info("patched AutoEvictAllowEviction", "name", item.GetName(), "value", "False")
-				patchedCount++
+				input.Logger.Info("Replaced DrbdOptions/AutoEvictAllowEviction value to False", "name", item.GetName())
 			}
 		}
 
 		// Check for thin pool granularity
 		if propKey == "StorDriver/internal/lvmthin/thinPoolGranularity" {
 			thinPoolExistence = true
-			logger.Debug("found thin pool granularity setting", "name", item.GetName())
 		}
 	}
-
-	logger.Info("propscontainer processing complete", "total", len(propsList.Items), "patched", patchedCount)
 
 	// Handle thin provisioning setting
 	if thinPoolExistence {
-		if err := enableThinProvisioning(ctx, cl, moduleName); err != nil {
-			logger.Info("failed to enable thin provisioning", "err", err)
-		} else {
-			logger.Info("enabled thin provisioning", "module", moduleName)
-		}
-	} else {
-		logger.Debug("thin pool granularity not found, skipping thin provisioning enablement")
-	}
-
-	return nil
-}
-
-// patchPropsContainer patches a propscontainer with new prop_value
-func patchPropsContainer(ctx context.Context, cl client.Client, item *unstructured.Unstructured, newValue string) error {
-	patch := map[string]interface{}{
-		"spec": map[string]interface{}{
-			"prop_value": newValue,
-		},
-	}
-
-	patchObj := &unstructured.Unstructured{Object: patch}
-	patchObj.SetGroupVersionKind(item.GroupVersionKind())
-	patchObj.SetName(item.GetName())
-
-	return cl.Patch(ctx, patchObj, client.MergeFrom(item))
-}
-
-// enableThinProvisioning enables thin provisioning in module config
-func enableThinProvisioning(ctx context.Context, cl client.Client, moduleName string) error {
-	// Get existing module config
-	modCfg := &unstructured.Unstructured{}
-	modCfg.SetGroupVersionKind(schema.GroupVersionKind{
-		Group:   "deckhouse.io",
-		Version: "v1alpha1",
-		Kind:    "ModuleConfig",
-	})
-	modCfg.SetName(moduleName)
-
-	// Try to get existing config first
-	if err := cl.Get(ctx, client.ObjectKey{Name: moduleName}, modCfg); err != nil {
-		// If not found, create new one
 		patch := map[string]interface{}{
 			"spec": map[string]interface{}{
 				"settings": map[string]interface{}{
@@ -142,22 +230,25 @@ func enableThinProvisioning(ctx context.Context, cl client.Client, moduleName st
 				},
 			},
 		}
-		modCfg.SetUnstructuredContent(patch)
-		return cl.Create(ctx, modCfg)
+		modCfg := &unstructured.Unstructured{Object: patch}
+		modCfg.SetGroupVersionKind(schema.GroupVersionKind{
+			Group:   "deckhouse.io",
+			Version: "v1alpha1",
+			Kind:    "ModuleConfig",
+		})
+		modCfg.SetName("sds-replicated-volume")
+
+		// Use empty object as base for MergeFrom
+		base := &unstructured.Unstructured{}
+		base.SetGroupVersionKind(modCfg.GroupVersionKind())
+		base.SetName(modCfg.GetName())
+
+		if err := cl.Patch(ctx, modCfg, client.MergeFrom(base)); err != nil {
+			input.Logger.Info("Failed to patch moduleconfig for thin provisioning", "err", err)
+		} else {
+			input.Logger.Info("Thin pools present, switching enableThinProvisioning on")
+		}
 	}
 
-	// Update existing config
-	patch := map[string]interface{}{
-		"spec": map[string]interface{}{
-			"settings": map[string]interface{}{
-				"enableThinProvisioning": true,
-			},
-		},
-	}
-
-	patchObj := &unstructured.Unstructured{Object: patch}
-	patchObj.SetGroupVersionKind(modCfg.GroupVersionKind())
-	patchObj.SetName(moduleName)
-
-	return cl.Patch(ctx, patchObj, client.MergeFrom(modCfg))
+	return nil
 }
