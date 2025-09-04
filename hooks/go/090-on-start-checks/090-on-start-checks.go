0c3cec767e140a88267683f69019d6951c825573
Nikolay Demchuk <nikolay.demchuk@flant.com>
2025-09-04 20:24:40 +1000

    fix (added thin provisioning auto-disable if non-existent) Signed-off-by: Nikolay Demchuk <nikolay.demchuk@flant.com>

--- hooks/go/090-on-start-checks/090-on-start-checks.go
+++ hooks/go/090-on-start-checks/090-on-start-checks.go
@@ -156,7 +156,52 @@ func onStartChecks(ctx context.Context, input *pkg.HookInput) error {
 			}
 		}
 	} else {
-		input.Logger.Info("No thin pool granularity found, skipping thin provisioning enablement")
+		input.Logger.Info("No thin pool granularity found, checking if thin provisioning should be disabled")
+
+		// Check existing ModuleConfig for enableThinProvisioning setting
+		modCfg := &unstructured.Unstructured{}
+		modCfg.SetGroupVersionKind(schema.GroupVersionKind{
+			Group:   "deckhouse.io",
+			Version: "v1alpha1",
+			Kind:    "ModuleConfig",
+		})
+		modCfg.SetName("sds-replicated-volume")
+
+		err := cl.Get(ctx, client.ObjectKey{Name: "sds-replicated-volume"}, modCfg)
+		if err != nil {
+			if client.IgnoreNotFound(err) == nil {
+				input.Logger.Info("ModuleConfig not found, nothing to disable")
+			} else {
+				input.Logger.Info("Failed to get ModuleConfig", "err", err)
+				return err
+			}
+		} else {
+			// Check if enableThinProvisioning is currently true
+			enableThinProvisioning, found, _ := unstructured.NestedBool(modCfg.Object, "spec", "settings", "enableThinProvisioning")
+			if found && enableThinProvisioning {
+				// Disable thin provisioning
+				patch := map[string]interface{}{
+					"spec": map[string]interface{}{
+						"settings": map[string]interface{}{
+							"enableThinProvisioning": false,
+						},
+					},
+				}
+
+				patchBytes, err := json.Marshal(patch)
+				if err != nil {
+					input.Logger.Info("Failed to marshal patch for moduleconfig", "err", err)
+				} else {
+					if err := cl.Patch(ctx, modCfg, client.RawPatch(types.MergePatchType, patchBytes)); err != nil {
+						input.Logger.Info("Failed to patch moduleconfig", "err", err)
+					} else {
+						input.Logger.Info("Patched moduleconfig with thin provisioning disabled")
+					}
+				}
+			} else {
+				input.Logger.Info("Thin provisioning already disabled or not set")
+			}
+		}
 	}
 
 	return nil
