cdb5366764a86b1a19a888cebb8afe993fe3f41d
Aleksandr Stefurishin <aleksandr.stefurishin@flant.com>
2025-09-10 03:19:08 +0300

    crd fixes
    Signed-off-by: Aleksandr Stefurishin <aleksandr.stefurishin@flant.com>

--- api/v1alpha2/replicated_volume_replica.go
+++ api/v1alpha2/replicated_volume_replica.go
@@ -18,13 +18,13 @@ import (
 // +kubebuilder:selectablefield:JSONPath=.spec.replicatedVolumeName
 // +kubebuilder:printcolumn:name="Volume",type=string,JSONPath=".spec.replicatedVolumeName"
 // +kubebuilder:printcolumn:name="Node",type=string,JSONPath=".spec.nodeName"
-// +kubebuilder:printcolumn:name="Primary",type=boolean,JSONPath=".spec.primary"
-// +kubebuilder:printcolumn:name="Diskless",type=string,JSONPath=".spec.volumes[0].disk==\"\""
+// +kubebuilder:printcolumn:name="Primary",type=string,JSONPath=".status.conditions[?(@.type=='Primary')].status"
+// +kubebuilder:printcolumn:name="Diskless",type=string,JSONPath=".spec.volumes[0].disk==\"\" || .spec.volumes[0].disk==null"
 // +kubebuilder:printcolumn:name="Ready",type=string,JSONPath=".status.conditions[?(@.type=='Ready')].status"
 // +kubebuilder:printcolumn:name="ConfigurationAdjusted",type=string,JSONPath=".status.conditions[?(@.type=='ConfigurationAdjusted')].status"
 // +kubebuilder:printcolumn:name="InitialSync",type=string,JSONPath=".status.conditions[?(@.type=='InitialSync')].status"
 // +kubebuilder:printcolumn:name="Quorum",type=string,JSONPath=".status.conditions[?(@.type=='Quorum')].status"
-// +kubebuilder:printcolumn:name="Devices",type=string,JSONPath=".status.conditions[?(@.type=='DevicesReady')].status"
+// +kubebuilder:printcolumn:name="DevicesReady",type=string,JSONPath=".status.conditions[?(@.type=='DevicesReady')].status"
 // +kubebuilder:printcolumn:name="DiskIOSuspended",type=string,JSONPath=".status.conditions[?(@.type=='DiskIOSuspended')].status"
 // +kubebuilder:printcolumn:name="Age",type=date,JSONPath=".metadata.creationTimestamp"
 type ReplicatedVolumeReplica struct {
