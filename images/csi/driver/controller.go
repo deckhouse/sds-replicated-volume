bf2dec9b02b7e8a60f7433c8c84486f7a0d23802
Aleksandr Zimin <alexandr.zimin@flant.com>
2025-11-11 10:16:36 +0300

    initial
    Signed-off-by: Aleksandr Zimin <alexandr.zimin@flant.com>

---
+++ images/csi/driver/controller.go
@@ -0,0 +1,425 @@
+/*
+Copyright 2025 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package driver
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/container-storage-interface/spec/lib/go/csi"
+	"github.com/google/uuid"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	kerrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/resource"
+
+	"github.com/deckhouse/sds-replicated-volume/api/v1alpha2"
+	"github.com/deckhouse/sds-replicated-volume/images/csi/internal"
+	"github.com/deckhouse/sds-replicated-volume/images/csi/pkg/utils"
+)
+
+const (
+	ReplicasKey     = "replicated.csi.storage.deckhouse.io/replicas"
+	TopologyKey     = "replicated.csi.storage.deckhouse.io/topology"
+	VolumeAccessKey = "replicated.csi.storage.deckhouse.io/volume-access"
+	ZonesKey        = "replicated.csi.storage.deckhouse.io/zones"
+	SharedSecretKey = "replicated.csi.storage.deckhouse.io/shared-secret"
+)
+
+func (d *Driver) CreateVolume(ctx context.Context, request *csi.CreateVolumeRequest) (*csi.CreateVolumeResponse, error) {
+	traceID := uuid.New().String()
+
+	d.log.Trace(fmt.Sprintf("[CreateVolume][traceID:%s] ========== CreateVolume ============", traceID))
+	d.log.Trace(request.String())
+	d.log.Trace(fmt.Sprintf("[CreateVolume][traceID:%s] ========== CreateVolume ============", traceID))
+
+	if request.Parameters[internal.TypeKey] != internal.Replicated {
+		return nil, status.Error(codes.InvalidArgument, "Unsupported Storage Class type")
+	}
+
+	if len(request.Name) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "Volume Name cannot be empty")
+	}
+	volumeID := request.Name
+	if request.VolumeCapabilities == nil {
+		return nil, status.Error(codes.InvalidArgument, "Volume Capability cannot de empty")
+	}
+
+	BindingMode := request.Parameters[internal.BindingModeKey]
+	d.log.Info(fmt.Sprintf("[CreateVolume][traceID:%s][volumeID:%s] storage class BindingMode: %s", traceID, volumeID, BindingMode))
+
+	LvmType := request.Parameters[internal.LvmTypeKey]
+	d.log.Info(fmt.Sprintf("[CreateVolume][traceID:%s][volumeID:%s] storage class LvmType: %s", traceID, volumeID, LvmType))
+
+	if len(request.Parameters[internal.LVMVolumeGroupKey]) == 0 {
+		err := errors.New("no LVMVolumeGroups specified in a storage class's parameters")
+		d.log.Error(err, fmt.Sprintf("[CreateVolume][traceID:%s][volumeID:%s] no LVMVolumeGroups were found for the request: %+v", traceID, volumeID, request))
+		return nil, status.Errorf(codes.InvalidArgument, "no LVMVolumeGroups specified in a storage class's parameters")
+	}
+
+	storageClassLVGs, storageClassLVGParametersMap, err := utils.GetStorageClassLVGsAndParameters(ctx, d.cl, d.log, request.Parameters[internal.LVMVolumeGroupKey])
+	if err != nil {
+		d.log.Error(err, fmt.Sprintf("[CreateVolume][traceID:%s][volumeID:%s] error GetStorageClassLVGs", traceID, volumeID))
+		return nil, status.Errorf(codes.Internal, "error during GetStorageClassLVGs")
+	}
+
+	rvSize := resource.NewQuantity(request.CapacityRange.GetRequiredBytes(), resource.BinarySI)
+	d.log.Info(fmt.Sprintf("[CreateVolume][traceID:%s][volumeID:%s] ReplicatedVolume size: %s", traceID, volumeID, rvSize.String()))
+
+	// Parse parameters for ReplicatedVolume
+	replicas := byte(3) // default
+	if replicasStr, ok := request.Parameters[ReplicasKey]; ok {
+		if parsed, err := strconv.ParseUint(replicasStr, 10, 8); err == nil {
+			replicas = byte(parsed)
+		} else {
+			d.log.Warning(fmt.Sprintf("[CreateVolume][traceID:%s][volumeID:%s] Invalid replicas parameter, using default: 3", traceID, volumeID))
+			replicas = 3
+		}
+	}
+
+	topology := "Zonal" // default
+	if topo, ok := request.Parameters[TopologyKey]; ok {
+		topology = topo
+	}
+
+	volumeAccess := "PreferablyLocal" // default
+	if va, ok := request.Parameters[VolumeAccessKey]; ok {
+		volumeAccess = va
+	}
+
+	sharedSecret := uuid.New().String() // generate UUID as shared secret
+	if secret, ok := request.Parameters[SharedSecretKey]; ok && secret != "" {
+		sharedSecret = secret
+	}
+
+	var zones []string
+	if zonesStr, ok := request.Parameters[ZonesKey]; ok && zonesStr != "" {
+		// Parse zones from comma-separated string
+		for _, zone := range strings.Split(zonesStr, ",") {
+			zones = append(zones, strings.TrimSpace(zone))
+		}
+	}
+
+	// Build LVGRef list from storageClassLVGs
+	var lvgRefs []v1alpha2.LVGRef
+	for _, lvg := range storageClassLVGs {
+		lvgRef := v1alpha2.LVGRef{
+			Name: lvg.Name,
+		}
+		if LvmType == internal.LVMTypeThin {
+			if thinPoolName, ok := storageClassLVGParametersMap[lvg.Name]; ok {
+				lvgRef.ThinPoolName = thinPoolName
+			}
+		}
+		lvgRefs = append(lvgRefs, lvgRef)
+	}
+
+	// Build ReplicatedVolumeSpec
+	rvSpec := utils.BuildReplicatedVolumeSpec(
+		*rvSize,
+		LvmType,
+		lvgRefs,
+		replicas,
+		topology,
+		volumeAccess,
+		sharedSecret,
+		[]string{}, // publishRequested - will be set by controller
+		zones,
+	)
+
+	d.log.Info(fmt.Sprintf("[CreateVolume][traceID:%s][volumeID:%s] ReplicatedVolumeSpec: %+v", traceID, volumeID, rvSpec))
+
+	// Create ReplicatedVolume
+	d.log.Trace(fmt.Sprintf("[CreateVolume][traceID:%s][volumeID:%s] ------------ CreateReplicatedVolume start ------------", traceID, volumeID))
+	_, err = utils.CreateReplicatedVolume(ctx, d.cl, d.log, traceID, volumeID, rvSpec)
+	if err != nil {
+		if kerrors.IsAlreadyExists(err) {
+			d.log.Info(fmt.Sprintf("[CreateVolume][traceID:%s][volumeID:%s] ReplicatedVolume %s already exists. Skip creating", traceID, volumeID, volumeID))
+		} else {
+			d.log.Error(err, fmt.Sprintf("[CreateVolume][traceID:%s][volumeID:%s] error CreateReplicatedVolume", traceID, volumeID))
+			return nil, err
+		}
+	}
+	d.log.Trace(fmt.Sprintf("[CreateVolume][traceID:%s][volumeID:%s] ------------ CreateReplicatedVolume end ------------", traceID, volumeID))
+
+	// Wait for ReplicatedVolume to become ready
+	d.log.Trace(fmt.Sprintf("[CreateVolume][traceID:%s][volumeID:%s] start wait ReplicatedVolume", traceID, volumeID))
+	attemptCounter, err := utils.WaitForReplicatedVolumeReady(ctx, d.cl, d.log, traceID, volumeID)
+	if err != nil {
+		d.log.Error(err, fmt.Sprintf("[CreateVolume][traceID:%s][volumeID:%s] error WaitForReplicatedVolumeReady. Delete ReplicatedVolume %s", traceID, volumeID, volumeID))
+
+		deleteErr := utils.DeleteReplicatedVolume(ctx, d.cl, d.log, traceID, volumeID)
+		if deleteErr != nil {
+			d.log.Error(deleteErr, fmt.Sprintf("[CreateVolume][traceID:%s][volumeID:%s] error DeleteReplicatedVolume", traceID, volumeID))
+		}
+
+		d.log.Error(err, fmt.Sprintf("[CreateVolume][traceID:%s][volumeID:%s] error creating ReplicatedVolume", traceID, volumeID))
+		return nil, err
+	}
+	d.log.Trace(fmt.Sprintf("[CreateVolume][traceID:%s][volumeID:%s] finish wait ReplicatedVolume, attempt counter = %d", traceID, volumeID, attemptCounter))
+
+	// Build volume context
+	volumeCtx := make(map[string]string, len(request.Parameters))
+	for k, v := range request.Parameters {
+		volumeCtx[k] = v
+	}
+	volumeCtx[internal.ReplicatedVolumeNameKey] = volumeID
+
+	d.log.Info(fmt.Sprintf("[CreateVolume][traceID:%s][volumeID:%s] Volume created successfully. volumeCtx: %+v", traceID, volumeID, volumeCtx))
+
+	return &csi.CreateVolumeResponse{
+		Volume: &csi.Volume{
+			CapacityBytes: request.CapacityRange.GetRequiredBytes(),
+			VolumeId:      request.Name,
+			VolumeContext: volumeCtx,
+			ContentSource: request.VolumeContentSource,
+		},
+	}, nil
+}
+
+func (d *Driver) DeleteVolume(ctx context.Context, request *csi.DeleteVolumeRequest) (*csi.DeleteVolumeResponse, error) {
+	traceID := uuid.New().String()
+	d.log.Info(fmt.Sprintf("[DeleteVolume][traceID:%s] ========== Start DeleteVolume ============", traceID))
+	if len(request.VolumeId) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "Volume ID cannot be empty")
+	}
+
+	err := utils.DeleteReplicatedVolume(ctx, d.cl, d.log, traceID, request.VolumeId)
+	if err != nil {
+		d.log.Error(err, "error DeleteReplicatedVolume")
+		return nil, err
+	}
+	d.log.Info(fmt.Sprintf("[DeleteVolume][traceID:%s][volumeID:%s] Volume deleted successfully", traceID, request.VolumeId))
+	d.log.Info(fmt.Sprintf("[DeleteVolume][traceID:%s] ========== END DeleteVolume ============", traceID))
+	return &csi.DeleteVolumeResponse{}, nil
+}
+
+func (d *Driver) ControllerPublishVolume(ctx context.Context, request *csi.ControllerPublishVolumeRequest) (*csi.ControllerPublishVolumeResponse, error) {
+	traceID := uuid.New().String()
+	d.log.Info(fmt.Sprintf("[ControllerPublishVolume][traceID:%s] ========== ControllerPublishVolume ============", traceID))
+	d.log.Trace(request.String())
+
+	if request.VolumeId == "" {
+		return nil, status.Error(codes.InvalidArgument, "Volume ID cannot be empty")
+	}
+	if request.NodeId == "" {
+		return nil, status.Error(codes.InvalidArgument, "Node ID cannot be empty")
+	}
+
+	volumeID := request.VolumeId
+	nodeID := request.NodeId
+
+	d.log.Info(fmt.Sprintf("[ControllerPublishVolume][traceID:%s][volumeID:%s][nodeID:%s] Adding node to publishRequested", traceID, volumeID, nodeID))
+
+	// Add node to publishRequested
+	err := utils.AddPublishRequested(ctx, d.cl, d.log, traceID, volumeID, nodeID)
+	if err != nil {
+		d.log.Error(err, fmt.Sprintf("[ControllerPublishVolume][traceID:%s][volumeID:%s][nodeID:%s] Failed to add node to publishRequested", traceID, volumeID, nodeID))
+		return nil, status.Errorf(codes.Internal, "Failed to add node to publishRequested: %v", err)
+	}
+
+	d.log.Info(fmt.Sprintf("[ControllerPublishVolume][traceID:%s][volumeID:%s][nodeID:%s] Waiting for node to appear in publishProvided", traceID, volumeID, nodeID))
+
+	// Wait for node to appear in publishProvided
+	err = utils.WaitForPublishProvided(ctx, d.cl, d.log, traceID, volumeID, nodeID)
+	if err != nil {
+		d.log.Error(err, fmt.Sprintf("[ControllerPublishVolume][traceID:%s][volumeID:%s][nodeID:%s] Failed to wait for publishProvided", traceID, volumeID, nodeID))
+		return nil, status.Errorf(codes.Internal, "Failed to wait for publishProvided: %v", err)
+	}
+
+	d.log.Info(fmt.Sprintf("[ControllerPublishVolume][traceID:%s][volumeID:%s][nodeID:%s] Volume published successfully", traceID, volumeID, nodeID))
+	d.log.Info(fmt.Sprintf("[ControllerPublishVolume][traceID:%s] ========== END ControllerPublishVolume ============", traceID))
+
+	return &csi.ControllerPublishVolumeResponse{
+		PublishContext: map[string]string{
+			internal.ReplicatedVolumeNameKey: volumeID,
+		},
+	}, nil
+}
+
+func (d *Driver) ControllerUnpublishVolume(ctx context.Context, request *csi.ControllerUnpublishVolumeRequest) (*csi.ControllerUnpublishVolumeResponse, error) {
+	traceID := uuid.New().String()
+	d.log.Info(fmt.Sprintf("[ControllerUnpublishVolume][traceID:%s] ========== ControllerUnpublishVolume ============", traceID))
+	d.log.Trace(request.String())
+
+	if request.VolumeId == "" {
+		return nil, status.Error(codes.InvalidArgument, "Volume ID cannot be empty")
+	}
+	if request.NodeId == "" {
+		return nil, status.Error(codes.InvalidArgument, "Node ID cannot be empty")
+	}
+
+	volumeID := request.VolumeId
+	nodeID := request.NodeId
+
+	d.log.Info(fmt.Sprintf("[ControllerUnpublishVolume][traceID:%s][volumeID:%s][nodeID:%s] Removing node from publishRequested", traceID, volumeID, nodeID))
+
+	// Remove node from publishRequested
+	err := utils.RemovePublishRequested(ctx, d.cl, d.log, traceID, volumeID, nodeID)
+	if err != nil {
+		d.log.Error(err, fmt.Sprintf("[ControllerUnpublishVolume][traceID:%s][volumeID:%s][nodeID:%s] Failed to remove node from publishRequested", traceID, volumeID, nodeID))
+		return nil, status.Errorf(codes.Internal, "Failed to remove node from publishRequested: %v", err)
+	}
+
+	d.log.Info(fmt.Sprintf("[ControllerUnpublishVolume][traceID:%s][volumeID:%s][nodeID:%s] Waiting for node to disappear from publishProvided", traceID, volumeID, nodeID))
+
+	// Wait for node to disappear from publishProvided
+	err = utils.WaitForPublishRemoved(ctx, d.cl, d.log, traceID, volumeID, nodeID)
+	if err != nil {
+		d.log.Error(err, fmt.Sprintf("[ControllerUnpublishVolume][traceID:%s][volumeID:%s][nodeID:%s] Failed to wait for publishRemoved", traceID, volumeID, nodeID))
+		return nil, status.Errorf(codes.Internal, "Failed to wait for publishRemoved: %v", err)
+	}
+
+	d.log.Info(fmt.Sprintf("[ControllerUnpublishVolume][traceID:%s][volumeID:%s][nodeID:%s] Volume unpublished successfully", traceID, volumeID, nodeID))
+	d.log.Info(fmt.Sprintf("[ControllerUnpublishVolume][traceID:%s] ========== END ControllerUnpublishVolume ============", traceID))
+
+	return &csi.ControllerUnpublishVolumeResponse{}, nil
+}
+
+func (d *Driver) ValidateVolumeCapabilities(_ context.Context, _ *csi.ValidateVolumeCapabilitiesRequest) (*csi.ValidateVolumeCapabilitiesResponse, error) {
+	d.log.Info("call method ValidateVolumeCapabilities")
+	return nil, nil
+}
+
+func (d *Driver) ListVolumes(_ context.Context, _ *csi.ListVolumesRequest) (*csi.ListVolumesResponse, error) {
+	d.log.Info("call method ListVolumes")
+	return nil, nil
+}
+
+func (d *Driver) GetCapacity(_ context.Context, _ *csi.GetCapacityRequest) (*csi.GetCapacityResponse, error) {
+	d.log.Info("method GetCapacity")
+
+	// todo MaxSize one PV
+	// todo call volumeBindingMode: WaitForFirstConsumer
+
+	return &csi.GetCapacityResponse{
+		AvailableCapacity: 1000000,
+		MaximumVolumeSize: nil,
+		MinimumVolumeSize: nil,
+	}, nil
+}
+
+func (d *Driver) ControllerGetCapabilities(_ context.Context, _ *csi.ControllerGetCapabilitiesRequest) (*csi.ControllerGetCapabilitiesResponse, error) {
+	d.log.Info("method ControllerGetCapabilities")
+
+	var capabilities = []csi.ControllerServiceCapability_RPC_Type{
+		csi.ControllerServiceCapability_RPC_CREATE_DELETE_VOLUME,
+		csi.ControllerServiceCapability_RPC_CLONE_VOLUME,
+		csi.ControllerServiceCapability_RPC_GET_CAPACITY,
+		csi.ControllerServiceCapability_RPC_EXPAND_VOLUME,
+		csi.ControllerServiceCapability_RPC_PUBLISH_UNPUBLISH_VOLUME,
+		// TODO: Add snapshot support if needed
+		// csi.ControllerServiceCapability_RPC_CREATE_DELETE_SNAPSHOT,
+	}
+
+	csiCaps := make([]*csi.ControllerServiceCapability, len(capabilities))
+
+	for i, capability := range capabilities {
+		csiCaps[i] = &csi.ControllerServiceCapability{
+			Type: &csi.ControllerServiceCapability_Rpc{
+				Rpc: &csi.ControllerServiceCapability_RPC{
+					Type: capability,
+				},
+			},
+		}
+	}
+
+	return &csi.ControllerGetCapabilitiesResponse{
+		Capabilities: csiCaps,
+	}, nil
+}
+
+func (d *Driver) ControllerExpandVolume(ctx context.Context, request *csi.ControllerExpandVolumeRequest) (*csi.ControllerExpandVolumeResponse, error) {
+	traceID := uuid.New().String()
+
+	d.log.Info(fmt.Sprintf("[ControllerExpandVolume][traceID:%s] method ControllerExpandVolume", traceID))
+	d.log.Trace(fmt.Sprintf("[ControllerExpandVolume][traceID:%s] ========== ControllerExpandVolume ============", traceID))
+	d.log.Trace(request.String())
+	d.log.Trace(fmt.Sprintf("[ControllerExpandVolume][traceID:%s] ========== ControllerExpandVolume ============", traceID))
+
+	volumeID := request.GetVolumeId()
+	if len(volumeID) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "Volume id cannot be empty")
+	}
+
+	rv, err := utils.GetReplicatedVolume(ctx, d.cl, volumeID)
+	if err != nil {
+		d.log.Error(err, fmt.Sprintf("[ControllerExpandVolume][traceID:%s][volumeID:%s] error getting ReplicatedVolume", traceID, volumeID))
+		return nil, status.Errorf(codes.Internal, "error getting ReplicatedVolume: %s", err.Error())
+	}
+
+	resizeDelta, err := resource.ParseQuantity(internal.ResizeDelta)
+	if err != nil {
+		d.log.Error(err, fmt.Sprintf("[ControllerExpandVolume][traceID:%s][volumeID:%s] error ParseQuantity for ResizeDelta", traceID, volumeID))
+		return nil, err
+	}
+	d.log.Trace(fmt.Sprintf("[ControllerExpandVolume][traceID:%s][volumeID:%s] resizeDelta: %s", traceID, volumeID, resizeDelta.String()))
+	requestCapacity := resource.NewQuantity(request.CapacityRange.GetRequiredBytes(), resource.BinarySI)
+	d.log.Trace(fmt.Sprintf("[ControllerExpandVolume][traceID:%s][volumeID:%s] requestCapacity: %s", traceID, volumeID, requestCapacity.String()))
+
+	nodeExpansionRequired := true
+	if request.GetVolumeCapability().GetBlock() != nil {
+		nodeExpansionRequired = false
+	}
+	d.log.Info(fmt.Sprintf("[ControllerExpandVolume][traceID:%s][volumeID:%s] NodeExpansionRequired: %t", traceID, volumeID, nodeExpansionRequired))
+
+	// Check if resize is needed
+	currentSize := rv.Spec.Size
+	if currentSize.Value() > requestCapacity.Value()+resizeDelta.Value() || utils.AreSizesEqualWithinDelta(*requestCapacity, currentSize, resizeDelta) {
+		d.log.Warning(fmt.Sprintf("[ControllerExpandVolume][traceID:%s][volumeID:%s] requested size is less than or equal to the actual size of the volume include delta %s, no need to resize ReplicatedVolume %s, requested size: %s, actual size: %s, return NodeExpansionRequired: %t and CapacityBytes: %d", traceID, volumeID, resizeDelta.String(), volumeID, requestCapacity.String(), currentSize.String(), nodeExpansionRequired, currentSize.Value()))
+		return &csi.ControllerExpandVolumeResponse{
+			CapacityBytes:         currentSize.Value(),
+			NodeExpansionRequired: nodeExpansionRequired,
+		}, nil
+	}
+
+	d.log.Info(fmt.Sprintf("[ControllerExpandVolume][traceID:%s][volumeID:%s] start resize ReplicatedVolume", traceID, volumeID))
+	d.log.Info(fmt.Sprintf("[ControllerExpandVolume][traceID:%s][volumeID:%s] requested size: %s, actual size: %s", traceID, volumeID, requestCapacity.String(), currentSize.String()))
+	err = utils.ExpandReplicatedVolume(ctx, d.cl, rv, *requestCapacity)
+	if err != nil {
+		d.log.Error(err, fmt.Sprintf("[ControllerExpandVolume][traceID:%s][volumeID:%s] error updating ReplicatedVolume", traceID, volumeID))
+		return nil, status.Errorf(codes.Internal, "error updating ReplicatedVolume: %v", err)
+	}
+
+	// Wait for ReplicatedVolume to become ready after resize
+	attemptCounter, err := utils.WaitForReplicatedVolumeReady(ctx, d.cl, d.log, traceID, volumeID)
+	if err != nil {
+		d.log.Error(err, fmt.Sprintf("[ControllerExpandVolume][traceID:%s][volumeID:%s] error WaitForReplicatedVolumeReady", traceID, volumeID))
+		return nil, err
+	}
+	d.log.Info(fmt.Sprintf("[ControllerExpandVolume][traceID:%s][volumeID:%s] finish resize ReplicatedVolume, attempt counter = %d", traceID, volumeID, attemptCounter))
+
+	d.log.Info(fmt.Sprintf("[ControllerExpandVolume][traceID:%s][volumeID:%s] Volume expanded successfully", traceID, volumeID))
+
+	return &csi.ControllerExpandVolumeResponse{
+		CapacityBytes:         request.CapacityRange.RequiredBytes,
+		NodeExpansionRequired: nodeExpansionRequired,
+	}, nil
+}
+
+func (d *Driver) ControllerGetVolume(_ context.Context, _ *csi.ControllerGetVolumeRequest) (*csi.ControllerGetVolumeResponse, error) {
+	d.log.Info(" call method ControllerGetVolume")
+	return &csi.ControllerGetVolumeResponse{}, nil
+}
+
+func (d *Driver) ControllerModifyVolume(_ context.Context, _ *csi.ControllerModifyVolumeRequest) (*csi.ControllerModifyVolumeResponse, error) {
+	d.log.Info(" call method ControllerModifyVolume")
+	return &csi.ControllerModifyVolumeResponse{}, nil
+}
---
+++ images/csi/go.mod
@@ -0,0 +1,93 @@
+module github.com/deckhouse/sds-replicated-volume/images/csi
+
+go 1.24.10
+
+require (
+	github.com/container-storage-interface/spec v1.10.0
+	github.com/deckhouse/sds-node-configurator/api v0.0.0-20250917090813-2f0c8b6a607f
+	github.com/deckhouse/sds-replicated-volume/api v0.0.0-20250907192450-6e1330e9e380
+	github.com/go-logr/logr v1.4.3
+	github.com/golang/protobuf v1.5.4
+	github.com/google/uuid v1.6.0
+	github.com/onsi/ginkgo/v2 v2.27.2
+	github.com/onsi/gomega v1.38.2
+	github.com/stretchr/testify v1.11.1
+	golang.org/x/sync v0.17.0
+	golang.org/x/sys v0.37.0
+	google.golang.org/grpc v1.72.1
+	gopkg.in/yaml.v2 v2.4.0
+	k8s.io/api v0.34.0
+	k8s.io/apiextensions-apiserver v0.34.0
+	k8s.io/apimachinery v0.34.1
+	k8s.io/client-go v0.34.0
+	k8s.io/klog/v2 v2.130.1
+	k8s.io/mount-utils v0.31.0
+	k8s.io/utils v0.0.0-20250820121507-0af2bda4dd1d
+	sigs.k8s.io/controller-runtime v0.22.1
+)
+
+require (
+	github.com/Masterminds/semver/v3 v3.4.0 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/emicklei/go-restful/v3 v3.13.0 // indirect
+	github.com/evanphx/json-patch/v5 v5.9.11 // indirect
+	github.com/fxamacker/cbor/v2 v2.9.0 // indirect
+	github.com/go-openapi/jsonpointer v0.22.0 // indirect
+	github.com/go-openapi/jsonreference v0.21.1 // indirect
+	github.com/go-openapi/swag v0.24.1 // indirect
+	github.com/go-openapi/swag/cmdutils v0.24.0 // indirect
+	github.com/go-openapi/swag/conv v0.24.0 // indirect
+	github.com/go-openapi/swag/fileutils v0.24.0 // indirect
+	github.com/go-openapi/swag/jsonname v0.24.0 // indirect
+	github.com/go-openapi/swag/jsonutils v0.24.0 // indirect
+	github.com/go-openapi/swag/loading v0.24.0 // indirect
+	github.com/go-openapi/swag/mangling v0.24.0 // indirect
+	github.com/go-openapi/swag/netutils v0.24.0 // indirect
+	github.com/go-openapi/swag/stringutils v0.24.0 // indirect
+	github.com/go-openapi/swag/typeutils v0.24.0 // indirect
+	github.com/go-openapi/swag/yamlutils v0.24.0 // indirect
+	github.com/go-task/slim-sprig/v3 v3.0.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/google/gnostic-models v0.7.0 // indirect
+	github.com/google/go-cmp v0.7.0 // indirect
+	github.com/google/pprof v0.0.0-20250403155104-27863c87afa6 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/mailru/easyjson v0.9.0 // indirect
+	github.com/moby/sys/mountinfo v0.7.2 // indirect
+	github.com/moby/sys/userns v0.1.0 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.3-0.20250322232337-35a7c28c31ee // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/opencontainers/runc v1.2.8 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	github.com/spf13/pflag v1.0.7 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
+	go.yaml.in/yaml/v2 v2.4.2 // indirect
+	go.yaml.in/yaml/v3 v3.0.4 // indirect
+	golang.org/x/exp v0.0.0-20251023183803-a4bb9ffd2546 // indirect
+	golang.org/x/mod v0.29.0 // indirect
+	golang.org/x/net v0.46.0 // indirect
+	golang.org/x/oauth2 v0.31.0 // indirect
+	golang.org/x/term v0.36.0 // indirect
+	golang.org/x/text v0.30.0 // indirect
+	golang.org/x/time v0.13.0 // indirect
+	golang.org/x/tools v0.38.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250303144028-a0af3efb3deb // indirect
+	google.golang.org/protobuf v1.36.9 // indirect
+	gopkg.in/evanphx/json-patch.v4 v4.12.0 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	k8s.io/kube-openapi v0.0.0-20250909170358-d67c058d9372 // indirect
+	sigs.k8s.io/json v0.0.0-20250730193827-2d320260d730 // indirect
+	sigs.k8s.io/randfill v1.0.0 // indirect
+	sigs.k8s.io/structured-merge-diff/v6 v6.3.0 // indirect
+	sigs.k8s.io/yaml v1.6.0 // indirect
+)
+
+replace github.com/imdario/mergo => github.com/imdario/mergo v0.3.16
+
+replace github.com/deckhouse/sds-replicated-volume/api => ../../api
+
+replace github.com/deckhouse/sds-replicated-volume/lib/go/common => ../../lib/go/common
