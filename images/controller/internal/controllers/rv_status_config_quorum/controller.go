98f12c6f27c30a8e7d108866747c95757a071403
«Mikahil <mikhail.sklyarevskiy@flant.com>
2025-11-27 15:54:48 +0300

    refctot

--- images/controller/internal/controllers/rv_status_config_quorum/controller.go
+++ images/controller/internal/controllers/rv_status_config_quorum/controller.go
@@ -2,7 +2,6 @@ package rvrdiskfulcount // TODO change package if need
 
 import (
 	"github.com/deckhouse/sds-replicated-volume/api/v1alpha3"
-	"k8s.io/client-go/util/workqueue"
 	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
@@ -10,16 +9,12 @@ import (
 
 func BuildController(mgr manager.Manager) error {
 
-	// TODO issues/333 your global dependencies
-	var rec = &Reconciler{
+	rec := &Reconciler{
 		cl:  mgr.GetClient(),
 		rdr: mgr.GetAPIReader(),
 		sch: mgr.GetScheme(),
 		log: mgr.GetLogger().WithName("controller_rv_status_config_quorum"),
 	}
-
-	type TReq = Request
-	type TQueue = workqueue.TypedRateLimitingInterface[TReq]
 
 	return builder.ControllerManagedBy(mgr).
 		Named("rv_status_config_quorum_controller").
