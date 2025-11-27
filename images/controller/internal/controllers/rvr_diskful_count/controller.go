6a6f9291b2df6ce7fc865af6a2bbd264081ca1b9
Pavel Karpov <pavel.karpov@flant.com>
2025-11-27 15:07:37 +0300

    init
    Signed-off-by: Pavel Karpov <pavel.karpov@flant.com>

--- images/controller/internal/controllers/rvr_diskful_count/controller.go
+++ images/controller/internal/controllers/rvr_diskful_count/controller.go
@@ -1,75 +1,32 @@
 package rvrdiskfulcount
 
 import (
-	"context"
-	"log/slog"
-
-	u "github.com/deckhouse/sds-common-lib/utils"
 	"github.com/deckhouse/sds-replicated-volume/api/v1alpha3"
-	e "github.com/deckhouse/sds-replicated-volume/images/controller/internal/errors"
-	"k8s.io/client-go/util/workqueue"
 	"sigs.k8s.io/controller-runtime/pkg/builder"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 )
 
 func BuildController(mgr manager.Manager) error {
 
-	// TODO issues/333 your global dependencies
-	var rec = &Reconciler{
-		cl:     mgr.GetClient(),
-		rdr:    mgr.GetAPIReader(),
-		sch:    mgr.GetScheme(),
-		log:    slog.Default(),
-		logAlt: mgr.GetLogger(),
+	nameController := "rvr_diskful_count_controller"
+
+	r := &Reconciler{
+		cl:  mgr.GetClient(),
+		log: mgr.GetLogger().WithName(nameController).WithName("Reconciler"),
+	}
+	h := &Handler{
+		cl:  mgr.GetClient(),
+		log: mgr.GetLogger().WithName(nameController).WithName("Handler"),
 	}
 
-	type TReq = Request
-	type TQueue = workqueue.TypedRateLimitingInterface[TReq]
-
-	err := builder.TypedControllerManagedBy[TReq](mgr).
-		Named("rvr_diskful_count_controller").
+	return builder.ControllerManagedBy(mgr).
+		Named(nameController).
+		Watches(
+			&v1alpha3.ReplicatedVolumeReplica{},
+			handler.EnqueueRequestsFromMapFunc(h.HandleReplicatedVolumeReplica)).
 		Watches(
 			&v1alpha3.ReplicatedVolume{},
-			&handler.TypedFuncs[client.Object, TReq]{
-				CreateFunc: func(
-					ctx context.Context,
-					ce event.TypedCreateEvent[client.Object],
-					q TQueue,
-				) {
-					// TODO issues/333 filter events here
-				},
-				UpdateFunc: func(
-					ctx context.Context,
-					ue event.TypedUpdateEvent[client.Object],
-					q TQueue,
-				) {
-					// TODO issues/333 filter events here
-				},
-				DeleteFunc: func(
-					ctx context.Context,
-					de event.TypedDeleteEvent[client.Object],
-					q TQueue,
-				) {
-					// TODO issues/333 filter events here
-				},
-				GenericFunc: func(
-					ctx context.Context,
-					ge event.TypedGenericEvent[client.Object],
-					q TQueue,
-				) {
-					// TODO issues/333 filter events here
-				},
-			}).
-		Complete(rec)
-
-	if err != nil {
-		// TODO issues/333 log errors early
-		// TODO issues/333 use typed errors
-		return u.LogError(rec.log, e.ErrUnknownf("building controller: %w", err))
-	}
-
-	return nil
+			handler.EnqueueRequestsFromMapFunc(h.HandleReplicatedVolume)).
+		Complete(r)
 }
--- images/controller/internal/controllers/rvr_diskful_count/reconciler.go
+++ images/controller/internal/controllers/rvr_diskful_count/reconciler.go
@@ -2,38 +2,62 @@ package rvrdiskfulcount
 
 import (
 	"context"
-	"log/slog"
 
-	e "github.com/deckhouse/sds-replicated-volume/images/controller/internal/errors"
+	"github.com/deckhouse/sds-replicated-volume/api/v1alpha3"
 	"github.com/go-logr/logr"
-	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
 type Reconciler struct {
-	cl     client.Client
-	rdr    client.Reader
-	sch    *runtime.Scheme
-	log    *slog.Logger // TODO issues/333 choose one logger of (both work via slogh)
-	logAlt logr.Logger
+	cl  client.Client
+	log logr.Logger
 }
 
-var _ reconcile.TypedReconciler[Request] = &Reconciler{}
+type Request = reconcile.Request
 
-func (r *Reconciler) Reconcile(
-	ctx context.Context,
-	req Request,
-) (reconcile.Result, error) {
-	// TODO issues/333 reconcile requests here
-	switch typedReq := req.(type) {
-	case AddFirstRequest:
-		return reconcile.Result{}, e.ErrNotImplemented
+var _ reconcile.Reconciler = (*Reconciler)(nil)
 
-	case AddSubsequentRequest:
-		return reconcile.Result{}, e.ErrNotImplemented
-	default:
-		r.log.Error("unknown req type", "typedReq", typedReq)
-		return reconcile.Result{}, e.ErrNotImplemented
+func (r *Reconciler) Reconcile(ctx context.Context, req Request) (reconcile.Result, error) {
+	log := r.log.WithName("Reconcile").WithValues("req", req)
+	log.Info("Reconciling")
+
+	// Determine object type: try to get both types and see which one exists
+	rvr := &v1alpha3.ReplicatedVolumeReplica{}
+	errRVR := r.cl.Get(ctx, req.NamespacedName, rvr)
+	if errRVR == nil {
+		// This is ReplicatedVolumeReplica
+		log.Info("Reconciling ReplicatedVolumeReplica", "name", req.Name)
+		return r.reconcileRVR(ctx, rvr)
 	}
+
+	rv := &v1alpha3.ReplicatedVolume{}
+	errRV := r.cl.Get(ctx, req.NamespacedName, rv)
+	if errRV == nil {
+		// This is ReplicatedVolume
+		log.Info("Reconciling ReplicatedVolume", "name", req.Name)
+		return r.reconcileRV(ctx, rv)
+	}
+
+	// If both objects are not found, they might have been deleted
+	if client.IgnoreNotFound(errRVR) == nil && client.IgnoreNotFound(errRV) == nil {
+		log.Info("Object not found, might be deleted", "name", req.Name)
+		return reconcile.Result{}, nil
+	}
+
+	// If there was another error, return it (prioritize the first error if it's not NotFound)
+	if client.IgnoreNotFound(errRVR) != nil {
+		return reconcile.Result{}, errRVR
+	}
+	return reconcile.Result{}, errRV
+}
+
+func (r *Reconciler) reconcileRVR(ctx context.Context, rvr *v1alpha3.ReplicatedVolumeReplica) (reconcile.Result, error) {
+	// TODO: implement logic for ReplicatedVolumeReplica
+	return reconcile.Result{}, nil
+}
+
+func (r *Reconciler) reconcileRV(ctx context.Context, rv *v1alpha3.ReplicatedVolume) (reconcile.Result, error) {
+	// TODO: implement logic for ReplicatedVolume
+	return reconcile.Result{}, nil
 }
