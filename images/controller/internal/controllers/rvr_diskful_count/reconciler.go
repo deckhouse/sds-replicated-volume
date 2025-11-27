a0043bf099dac2c1277edf080ba2411b02b76ccd
Pavel Karpov <pavel.karpov@flant.com>
2025-11-27 17:15:30 +0300

    add getDiskfulReplicaCount
    Signed-off-by: Pavel Karpov <pavel.karpov@flant.com>

--- images/controller/internal/controllers/rvr_diskful_count/reconciler.go
+++ images/controller/internal/controllers/rvr_diskful_count/reconciler.go
@@ -2,7 +2,10 @@ package rvrdiskfulcount
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/deckhouse/sds-replicated-volume/api/v1alpha1"
+	"github.com/deckhouse/sds-replicated-volume/api/v1alpha3"
 	"github.com/go-logr/logr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
@@ -19,8 +22,63 @@ var _ reconcile.Reconciler = (*Reconciler)(nil)
 
 func (r *Reconciler) Reconcile(ctx context.Context, req Request) (reconcile.Result, error) {
 	// always will come an event on ReplicatedVolume, even if the event happened on ReplicatedVolumeReplica
+
 	log := r.log.WithName("Reconcile").WithValues("req", req)
 	log.Info("Reconciling")
 
+	// Получаем объект ReplicatedVolume
+	rv := &v1alpha3.ReplicatedVolume{}
+	err := r.cl.Get(ctx, client.ObjectKey{Name: req.Name}, rv)
+	if err != nil {
+		if client.IgnoreNotFound(err) == nil {
+			log.Info("ReplicatedVolume not found, ignoring reconcile request")
+			return reconcile.Result{}, nil
+		}
+		return reconcile.Result{}, fmt.Errorf("getting ReplicatedVolume: %w", err)
+	}
+
+	if rv.DeletionTimestamp != nil {
+		log.Info("ReplicatedVolume is being deleted, ignoring reconcile request")
+		return reconcile.Result{}, nil
+	}
+
+	// Получаем количество diskful реплик
+	diskfulCount, err := getDiskfulReplicaCount(ctx, r.cl, rv)
+	if err != nil {
+		return reconcile.Result{}, fmt.Errorf("getting diskful replica count: %w", err)
+	}
+	log.V(4).Info("Calculated diskful replica count", "count", diskfulCount)
+
 	return reconcile.Result{}, nil
+}
+
+// getDiskfulReplicaCount получает количество diskful реплик на основе ReplicatedStorageClass.
+//
+// Если replication = None, возвращает 1; если replication = Availability, возвращает 2;
+// если replication = ConsistencyAndAvailability, возвращает 3.
+func getDiskfulReplicaCount(ctx context.Context, cl client.Client, rv *v1alpha3.ReplicatedVolume) (int, error) {
+	// Получаем имя ReplicatedStorageClass из ReplicatedVolume
+	rscName := rv.Spec.ReplicatedStorageClassName
+	if rscName == "" {
+		return 0, fmt.Errorf("ReplicatedVolume has empty ReplicatedStorageClassName")
+	}
+
+	// Получаем объект ReplicatedStorageClass
+	rsc := &v1alpha1.ReplicatedStorageClass{}
+	err := cl.Get(ctx, client.ObjectKey{Name: rscName}, rsc)
+	if err != nil {
+		return 0, fmt.Errorf("getting ReplicatedStorageClass %s: %w", rscName, err)
+	}
+
+	// Определяем количество diskful реплик на основе replication
+	switch rsc.Spec.Replication {
+	case "None":
+		return 1, nil
+	case "Availability":
+		return 2, nil
+	case "ConsistencyAndAvailability":
+		return 3, nil
+	default:
+		return 0, fmt.Errorf("unknown replication value: %s", rsc.Spec.Replication)
+	}
 }
