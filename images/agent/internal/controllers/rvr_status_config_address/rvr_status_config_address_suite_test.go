360a12d7195c1201a9cee493073fff72934521a7
Anton Sergunov <anton.sergunov@flant.com>
2025-12-01 11:11:35 +0600

    initial commit
    Signed-off-by: Anton Sergunov <anton.sergunov@flant.com>

---
+++ images/agent/internal/controllers/rvr_status_config_address/rvr_status_config_address_suite_test.go
@@ -0,0 +1,13 @@
+package rvrstatusconfigaddress_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestRvrStatusConfigAddress(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "RvrStatusConfigAddress Suite")
+}
