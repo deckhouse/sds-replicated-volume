1df2ddba5a139353560456d973ed5e180192ce40
Aleksandr Stefurishin <aleksandr.stefurishin@flant.com>
2025-12-01 16:22:00 +0300

    spec
    Signed-off-by: Aleksandr Stefurishin <aleksandr.stefurishin@flant.com>

--- api/v1alpha3/replicated_volume_replica.go
+++ api/v1alpha3/replicated_volume_replica.go
@@ -5,7 +5,6 @@ import (
 	"strings"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/fields"
 )
 
 // +k8s:deepcopy-gen=true
@@ -37,10 +36,6 @@ type ReplicatedVolumeReplica struct {
 	Status *ReplicatedVolumeReplicaStatus `json:"status,omitempty" patchStrategy:"merge"`
 }
 
-func (rvr *ReplicatedVolumeReplica) NodeNameSelector(nodeName string) fields.Selector {
-	return fields.OneTermEqualSelector("spec.nodeName", nodeName)
-}
-
 // +k8s:deepcopy-gen=true
 type ReplicatedVolumeReplicaSpec struct {
 	// +kubebuilder:validation:Required
@@ -55,8 +50,9 @@ type ReplicatedVolumeReplicaSpec struct {
 	// +kubebuilder:validation:MaxLength=253
 	NodeName string `json:"nodeName,omitempty"`
 
+	// +kubebuilder:validation:Required
 	// +kubebuilder:validation:Enum=Diskful;Access;TieBreaker
-	Type string `json:"type,omitempty"`
+	Type string `json:"type"`
 }
 
 // +k8s:deepcopy-gen=true
