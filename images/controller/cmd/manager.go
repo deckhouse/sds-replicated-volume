ad3309c336b277dd0ddd7585db151755cb7bd181
Pavel Karpov <pavel.karpov@flant.com>
2025-12-04 16:36:30 +0300

    add scheme v1alpha1
    Signed-off-by: Pavel Karpov <pavel.karpov@flant.com>

--- images/controller/cmd/manager.go
+++ images/controller/cmd/manager.go
@@ -32,6 +32,7 @@ import (
 
 	u "github.com/deckhouse/sds-common-lib/utils"
 	snc "github.com/deckhouse/sds-node-configurator/api/v1alpha1"
+	"github.com/deckhouse/sds-replicated-volume/api/v1alpha1"
 	"github.com/deckhouse/sds-replicated-volume/api/v1alpha3"
 	"github.com/deckhouse/sds-replicated-volume/images/controller/internal/controllers"
 )
@@ -87,6 +88,7 @@ func newScheme() (*runtime.Scheme, error) {
 	var schemeFuncs = []func(s *runtime.Scheme) error{
 		corev1.AddToScheme,
 		storagev1.AddToScheme,
+		v1alpha1.AddToScheme,
 		v1alpha3.AddToScheme,
 		snc.AddToScheme,
 	}
