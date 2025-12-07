0f1bdb68405d970b386f52a022e3b6b4ca49ecbb
Aleksandr Stefurishin <aleksandr.stefurishin@flant.com>
2025-12-07 16:51:05 +0300

    unify approach to configs, controller entrypoint
    Signed-off-by: Aleksandr Stefurishin <aleksandr.stefurishin@flant.com>

--- images/agent/internal/controllers/registry.go
+++ images/agent/internal/controllers/registry.go
@@ -21,20 +21,19 @@ import (
 
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 
-	"github.com/deckhouse/sds-replicated-volume/images/agent/internal/config"
 	rvrstatusconfigaddress "github.com/deckhouse/sds-replicated-volume/images/agent/internal/controllers/rvr_status_config_address"
 )
 
-var registry []func(mgr manager.Manager, cfg config.Config) error
+var registry []func(mgr manager.Manager) error
 
 func init() {
 	registry = append(registry, rvrstatusconfigaddress.BuildController)
 	// ...
 }
 
-func BuildAll(mgr manager.Manager, cfg config.Config) error {
+func BuildAll(mgr manager.Manager) error {
 	for i, buildCtl := range registry {
-		err := buildCtl(mgr, cfg)
+		err := buildCtl(mgr)
 		if err != nil {
 			return fmt.Errorf("building controller %d: %w", i, err)
 		}
