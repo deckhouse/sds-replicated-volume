a7818fa614e942010af1a593b923fa18f37fdff1
Vyacheslav Voytenok <vyacheslav.voytenok@flant.com>
2025-12-09 22:27:19 +0700

    [controller] Implement rvr-tie-breaker-count (#358)
    Signed-off-by: Vyacheslav Voytenok <vyacheslav.voytenok@flant.com>
    Signed-off-by: Anton Sergunov <anton.sergunov@flant.com>
    Signed-off-by: Aleksandr Zimin <alexandr.zimin@flant.com>
    Co-authored-by: Anton Sergunov <anton.sergunov@flant.com>
    Co-authored-by: Aleksandr Zimin <alexandr.zimin@flant.com>

---
+++ images/controller/internal/controllers/rvr_tie_breaker_count/rvr_tie_breaker_count_suite_test.go
@@ -0,0 +1,44 @@
+/*
+Copyright 2025 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package rvrtiebreakercount_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/types"
+
+	"github.com/deckhouse/sds-replicated-volume/api/v1alpha3"
+)
+
+func TestRvrTieBreakerCount(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "RvrTieBreakerCount Suite")
+}
+
+func HaveTieBreakerCount(matcher types.GomegaMatcher) types.GomegaMatcher {
+	return WithTransform(func(list []v1alpha3.ReplicatedVolumeReplica) int {
+		tbCount := 0
+		for _, rvr := range list {
+			if rvr.Spec.Type == "TieBreaker" {
+				tbCount++
+			}
+		}
+		return tbCount
+	}, matcher)
+}
