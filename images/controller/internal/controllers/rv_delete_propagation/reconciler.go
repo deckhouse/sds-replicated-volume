33feae4a297a22f127a560864c0486d4cea78555
Aleksandr Stefurishin <aleksandr.stefurishin@flant.com>
2025-12-16 16:51:18 +0300

    migrate to single reconcilation key
    Signed-off-by: Aleksandr Stefurishin <aleksandr.stefurishin@flant.com>

--- images/controller/internal/controllers/rv_delete_propagation/reconciler.go
+++ images/controller/internal/controllers/rv_delete_propagation/reconciler.go
@@ -21,10 +21,7 @@ import (
 	"fmt"
 	"log/slog"
 
-	u "github.com/deckhouse/sds-common-lib/utils"
 	"github.com/deckhouse/sds-replicated-volume/api/v1alpha3"
-	e "github.com/deckhouse/sds-replicated-volume/images/controller/internal/errors"
-	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
@@ -34,9 +31,9 @@ type Reconciler struct {
 	log *slog.Logger
 }
 
-var _ reconcile.TypedReconciler[Request] = &Reconciler{}
+var _ reconcile.Reconciler = &Reconciler{}
 
-func NewReconciler(cl client.Client, rdr client.Reader, log *slog.Logger, nodeName string) *Reconciler {
+func NewReconciler(cl client.Client, log *slog.Logger) *Reconciler {
 	if log == nil {
 		log = slog.Default()
 	}
@@ -46,39 +43,39 @@ func NewReconciler(cl client.Client, rdr client.Reader, log *slog.Logger, nodeNa
 	}
 }
 
-func (r *Reconciler) OnRVCreateOrUpdate(
-	ctx context.Context,
-	rv *v1alpha3.ReplicatedVolume,
-	q TQueue,
-) {
-	if linkedRVRsNeedToBeDeleted(rv) {
-		q.Add(DeleteLinkedRVRsRequest{RVName: rv.Name})
-	}
-}
-
-func (r *Reconciler) Reconcile(
-	ctx context.Context,
-	req Request,
-) (reconcile.Result, error) {
+func (r *Reconciler) Reconcile(ctx context.Context, req reconcile.Request) (reconcile.Result, error) {
 	rv := &v1alpha3.ReplicatedVolume{}
-	if err := r.cl.Get(ctx, types.NamespacedName{Name: req.GetRVName()}, rv); err != nil {
+	if err := r.cl.Get(ctx, req.NamespacedName, rv); err != nil {
 		return reconcile.Result{}, fmt.Errorf("getting rv: %w", err)
 	}
 
 	log := r.log.With("rvName", rv.Name)
 
-	var handle func(ctx context.Context) error
-	switch typedReq := req.(type) {
-	case DeleteLinkedRVRsRequest:
-		handle = (&DeleteLinkedRVRsHandler{
-			cl:     r.cl,
-			log:    log.With("handler", "AddFinalizerHandler"),
-			rvName: rv.Name,
-		}).Handle
-	default:
-		r.log.Error("unknown req type", "typedReq", typedReq)
-		return reconcile.Result{}, e.ErrNotImplemented
+	if !linkedRVRsNeedToBeDeleted(rv) {
+		log.Debug("linked do not need to be deleted")
+		return reconcile.Result{}, nil
 	}
 
-	return reconcile.Result{}, u.LogError(log, handle(ctx))
+	rvrList := &v1alpha3.ReplicatedVolumeReplicaList{}
+	if err := r.cl.List(ctx, rvrList); err != nil {
+		return reconcile.Result{}, fmt.Errorf("listing rvrs: %w", err)
+	}
+
+	for i := range rvrList.Items {
+		rvr := &rvrList.Items[i]
+		if rvr.Spec.ReplicatedVolumeName == rv.Name && rvr.DeletionTimestamp == nil {
+			if err := r.cl.Delete(ctx, rvr); err != nil {
+				return reconcile.Result{}, fmt.Errorf("deleting rvr: %w", err)
+			}
+
+			log.Info("deleted rvr", "rvrName", rvr.Name)
+		}
+	}
+
+	log.Info("finished rvr deletion")
+	return reconcile.Result{}, nil
+}
+
+func linkedRVRsNeedToBeDeleted(rv *v1alpha3.ReplicatedVolume) bool {
+	return rv.DeletionTimestamp == nil
 }
