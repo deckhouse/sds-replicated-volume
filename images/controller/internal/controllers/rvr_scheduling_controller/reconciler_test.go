ef65b6df65650ee14c82f63723a3e626c5f6577b
Aleksandr Zimin <alexandr.zimin@flant.com>
2025-12-24 17:29:38 +0300

    fix linter
    Signed-off-by: Aleksandr Zimin <alexandr.zimin@flant.com>

--- images/controller/internal/controllers/rvr_scheduling_controller/reconciler_test.go
+++ images/controller/internal/controllers/rvr_scheduling_controller/reconciler_test.go
@@ -25,7 +25,6 @@ import (
 	"os"
 	"slices"
 
-	snc "github.com/deckhouse/sds-node-configurator/api/v1alpha1"
 	"github.com/go-logr/logr"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -40,6 +39,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
+	snc "github.com/deckhouse/sds-node-configurator/api/v1alpha1"
 	v1alpha1 "github.com/deckhouse/sds-replicated-volume/api/v1alpha1"
 	rvrschedulingcontroller "github.com/deckhouse/sds-replicated-volume/images/controller/internal/controllers/rvr_scheduling_controller"
 )
