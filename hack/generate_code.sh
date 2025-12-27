b944221ff3ec6488ca5a08499dc52b3b244ce992
David Magton <david.magton@flant.com>
2025-12-27 21:45:12 +0300

    api: add ReplicatedVolumeAttachment CRD + pin controller-gen v0.20.0
    Signed-off-by: David Magton <david.magton@flant.com>

--- hack/generate_code.sh
+++ hack/generate_code.sh
@@ -15,12 +15,12 @@
 # limitations under the License.
 
 # run from repository root with: 'bash hack/generate_code.sh'
-set -e
+set -euo pipefail
 cd api
 
 # crds
-go get sigs.k8s.io/controller-tools/cmd/controller-gen@v0.19
-go run sigs.k8s.io/controller-tools/cmd/controller-gen \
+go get sigs.k8s.io/controller-tools/cmd/controller-gen@v0.20
+go run sigs.k8s.io/controller-tools/cmd/controller-gen@v0.20 \
     object:headerFile=../hack/boilerplate.txt \
     crd paths=./v1alpha1 output:crd:dir=../crds \
     paths=./v1alpha1
