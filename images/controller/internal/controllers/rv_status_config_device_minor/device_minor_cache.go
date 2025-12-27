305290ce2c7df50e46174850c457caccf87b8cad
Aleksandr Stefurishin <aleksandr.stefurishin@flant.com>
2025-12-28 02:43:04 +0300

    device minor cache and tests
    Signed-off-by: Aleksandr Stefurishin <aleksandr.stefurishin@flant.com>

---
+++ images/controller/internal/controllers/rv_status_config_device_minor/device_minor_cache.go
@@ -0,0 +1,201 @@
+package rvstatusconfigdeviceminor
+
+import (
+	"errors"
+	"fmt"
+	"slices"
+)
+
+const MaxDeviceMinor DeviceMinor = 1_048_575 // 2^20-1
+
+type DeviceMinor int
+
+const DeviceMinorZero DeviceMinor = DeviceMinor(0)
+
+func NewDeviceMinor(val int) (DeviceMinor, bool) {
+	dm := DeviceMinor(val)
+	if dm < DeviceMinorZero || dm > MaxDeviceMinor {
+		return DeviceMinorZero, false
+	}
+	return dm, true
+}
+
+func (dm DeviceMinor) Increment() (DeviceMinor, bool) {
+	if dm == MaxDeviceMinor {
+		return MaxDeviceMinor, false
+	}
+	return dm + 1, true
+}
+
+func (dm DeviceMinor) Decrement() (DeviceMinor, bool) {
+	if dm == DeviceMinorZero {
+		return DeviceMinorZero, false
+	}
+	return dm - 1, true
+}
+
+type DeviceMinorCache struct {
+	byRVName map[string]DeviceMinor // values are unique
+	max      DeviceMinor            // maximum value in byRVName
+	released []DeviceMinor          // "holes" in values in byRVName, sorted
+}
+
+func NewDeviceMinorCache() *DeviceMinorCache {
+	return &DeviceMinorCache{
+		byRVName: map[string]DeviceMinor{},
+	}
+}
+
+func (c *DeviceMinorCache) Len() int {
+	return len(c.byRVName)
+}
+
+func (c *DeviceMinorCache) Max() DeviceMinor {
+	return c.max
+}
+
+func (c *DeviceMinorCache) Released() []DeviceMinor {
+	return slices.Clone(c.released)
+}
+
+func (c *DeviceMinorCache) Initialize(byRVName map[string]DeviceMinor) error {
+	// Validate
+
+	// It's important to ensure DM uniqueness, because [DeviceMinorCache.Release]
+	// depends on [DeviceMinorCache.max] value decrement.
+	// Allowing duplicates in would lead to a corrupted state.
+
+	// using sorted array instead of map to be able to detect holes
+	dms := make([]DeviceMinor, 0, len(byRVName))
+	rvNames := make([]string, 0, len(byRVName)) // same index with dms
+
+	for rvName, dm := range byRVName {
+		i, found := slices.BinarySearch(dms, dm)
+		if found {
+			return fmt.Errorf("rvs '%s' and '%s' have same device minor %d", rvNames[i], rvName, dm)
+		}
+
+		dms = slices.Insert(dms, i, dm)
+		rvNames = slices.Insert(rvNames, i, rvName)
+	}
+
+	// Clear state
+	c.byRVName = make(map[string]DeviceMinor, len(dms))
+	c.released = nil
+	c.max = DeviceMinorZero
+
+	// Update state
+	for i, dm := range dms {
+		c.byRVName[rvNames[i]] = dm
+
+		// search for the hole on the left
+		var holeStart DeviceMinor
+		if i > 0 {
+			holeStart, _ = dms[i-1].Increment()
+		}
+		for ; holeStart < dm; holeStart, _ = holeStart.Increment() {
+			// adding a hole
+			c.insertReleased(holeStart)
+		}
+	}
+	if len(dms) > 0 {
+		c.max = dms[len(dms)-1]
+	}
+	return nil
+}
+
+func (c *DeviceMinorCache) GetOrCreate(rvName string) (DeviceMinor, error) {
+	// initialize first item
+	if len(c.byRVName) == 0 {
+		c.addRVDM(rvName, c.max)
+		return c.max, nil
+	}
+
+	// get existing
+	if dm, ok := c.byRVName[rvName]; ok {
+		return dm, nil
+	}
+
+	// create - reuse released minors
+	if dm, ok := c.takeFirstReleased(); ok {
+		c.addRVDM(rvName, dm)
+		return dm, nil
+	}
+
+	// create - new
+	dm, ok := c.max.Increment()
+	if !ok {
+		return dm, errors.New("ran out of device minors")
+	}
+	c.addRVDM(rvName, dm)
+	return dm, nil
+}
+
+func (c *DeviceMinorCache) Release(rvName string) {
+	c.removeRVDM(rvName)
+}
+
+func (c *DeviceMinorCache) addRVDM(rvName string, dm DeviceMinor) {
+	c.byRVName[rvName] = dm
+	c.max = max(c.max, dm)
+}
+
+func (c *DeviceMinorCache) removeRVDM(rvName string) {
+	dm, ok := c.byRVName[rvName]
+	if !ok {
+		return
+	}
+
+	if dm == c.max {
+		// decrement c.max until non-hole value is met, or collection is empty
+		for {
+			c.max, ok = c.max.Decrement()
+			if !ok {
+				// it was the last element
+				break
+			}
+			if maxReleased, ok := c.maxReleased(); !ok || maxReleased != c.max {
+				// no hole
+				break
+			}
+			// removing a hole
+			c.takeLastReleased()
+		}
+	} else {
+		// adding a hole
+		c.insertReleased(dm)
+	}
+
+	delete(c.byRVName, rvName)
+}
+
+func (c *DeviceMinorCache) takeFirstReleased() (DeviceMinor, bool) {
+	if len(c.released) == 0 {
+		return DeviceMinorZero, false
+	}
+	dm := c.released[0]
+	c.released = c.released[1:]
+	return dm, true
+}
+
+func (c *DeviceMinorCache) maxReleased() (DeviceMinor, bool) {
+	if len(c.released) == 0 {
+		return DeviceMinorZero, false
+	}
+	return c.released[len(c.released)-1], true
+}
+
+func (c *DeviceMinorCache) takeLastReleased() (DeviceMinor, bool) {
+	if len(c.released) == 0 {
+		return DeviceMinorZero, false
+	}
+	last := c.released[len(c.released)-1]
+	c.released = c.released[:len(c.released)-1]
+	return last, true
+}
+
+func (c *DeviceMinorCache) insertReleased(dm DeviceMinor) {
+	// we never replace the existing value, so second return value doesn't matter
+	i, _ := slices.BinarySearch(c.released, dm)
+	c.released = slices.Insert(c.released, i, dm)
+}
---
+++ images/controller/internal/controllers/rv_status_config_device_minor/device_minor_cache_test.go
@@ -0,0 +1,207 @@
+package rvstatusconfigdeviceminor_test
+
+import (
+	"slices"
+	"testing"
+
+	. "github.com/deckhouse/sds-replicated-volume/images/controller/internal/controllers/rv_status_config_device_minor"
+)
+
+type testDeviceMinorCache struct {
+	*testing.T
+	*DeviceMinorCache
+}
+
+func TestDeviceMinorCache(t *testing.T) {
+	// TODO: check that holes are filled from the smallest
+	testDeviceMinorCache{t, NewDeviceMinorCache()}.
+		// []
+		expectEmpty().
+		// [a, b, c, d, e, f, g, h]
+		getOrCreate("a", 0, "").
+		getOrCreate("b", 1, "").
+		getOrCreate("c", 2, "").
+		getOrCreate("d", 3, "").
+		getOrCreate("e", 4, "").
+		getOrCreate("f", 5, "").
+		getOrCreate("g", 6, "").
+		getOrCreate("h", 7, "").
+		expect(8, 7, nil).
+		// -
+		getOrCreate("a", 0, "").
+		getOrCreate("b", 1, "").
+		getOrCreate("b", 1, "").
+		getOrCreate("h", 7, "").
+		getOrCreate("a", 0, "").
+		getOrCreate("a", 0, "").
+		expect(8, 7, nil).
+		// [_, b, c, d, e, f, g, h]
+		release("a").
+		expect(7, 7, holes(0)).
+		// -
+		release("x").
+		expect(7, 7, holes(0)).
+		// -
+		release("y").
+		expect(7, 7, holes(0)).
+		// [_, _, c, d, e, f, g, h]
+		release("b").
+		expect(6, 7, holes(0, 1)).
+		// [_, _, c, d, e, _, g, h]
+		release("f").
+		expect(5, 7, holes(0, 1, 5)).
+		// [_, _, c, d, e, _, _, h]
+		release("g").
+		expect(4, 7, holes(0, 1, 5, 6)).
+		// [_, _, c, d, e]
+		release("h").
+		expect(3, 4, holes(0, 1)).
+		// [a, _, c, d, e]
+		getOrCreate("a", 0, "").
+		expect(4, 4, holes(1)).
+		// [a, _, _, d, e]
+		release("c").
+		expect(3, 4, holes(1, 2)).
+		// [a, _, _, _, e]
+		release("d").
+		expect(2, 4, holes(1, 2, 3)).
+		// [_, _, _, _, e]
+		release("a").
+		expect(1, 4, holes(0, 1, 2, 3)).
+		// []
+		release("e").
+		expect(0, 0, nil).
+		// [a, _, _, _, e]
+		initialize(map[string]DeviceMinor{"a": 0, "e": 4}, "").
+		expect(2, 4, holes(1, 2, 3)).
+		// -
+		initialize(map[string]DeviceMinor{"a": 0, "e": 4}, "").
+		expect(2, 4, holes(1, 2, 3)).
+		// -
+		initialize(map[string]DeviceMinor{"a": 99, "e": 99}, "rvs 'a' and 'e' have same device minor 99").
+		expect(2, 4, holes(1, 2, 3)).
+		// [a, b, _, _, e]
+		getOrCreate("b", 1, "").
+		expect(3, 4, holes(2, 3)).
+		// [a, b, c, _, e]
+		getOrCreate("c", 2, "").
+		expect(4, 4, holes(3)).
+		// [a, b, c, d, e]
+		getOrCreate("d", 3, "").
+		expect(5, 4, nil).
+		// [a, b, c, d, e, f, g, h]
+		getOrCreate("f", 5, "").
+		getOrCreate("g", 6, "").
+		getOrCreate("h", 7, "").
+		expect(8, 7, nil).
+		// [A, B, C, _, _, F, G, H]
+		initialize(map[string]DeviceMinor{
+			"A": 0,
+			"B": 1,
+			"C": 2,
+			"F": 5,
+			"G": 6,
+			"H": 7,
+		}, "").
+		expect(6, 7, holes(3, 4)).
+		// -
+		getOrCreate("F", 5, "").
+		getOrCreate("H", 7, "").
+		getOrCreate("G", 6, "").
+		getOrCreate("F", 5, "").
+		getOrCreate("C", 2, "").
+		getOrCreate("B", 1, "").
+		getOrCreate("A", 0, "").
+		expect(6, 7, holes(3, 4)).
+		// [_, _, _, _, _, F]
+		initialize(map[string]DeviceMinor{"F": 5}, "").
+		expect(1, 5, holes(0, 1, 2, 3, 4)).
+		// -
+		getOrCreate("F", 5, "").
+		// []
+		cleanup()
+}
+
+func (tc testDeviceMinorCache) getOrCreate(rvName string, expectedDM DeviceMinor, expectedErr string) testDeviceMinorCache {
+	tc.Helper()
+	dm, err := tc.GetOrCreate(rvName)
+	if dm != expectedDM {
+		tc.Fatalf("expected GetOrCreate result to be %d, got %d", expectedDM, dm)
+	}
+	if !errIsExpected(err, expectedErr) {
+		tc.Fatalf("expected GetOrCreate error to be %s, got %v", expectedErr, err)
+	}
+	return tc
+}
+
+func (tc testDeviceMinorCache) release(rvName string) testDeviceMinorCache {
+	tc.Helper()
+	tc.Release(rvName)
+	return tc
+}
+
+func (tc testDeviceMinorCache) initialize(
+	byRVName map[string]DeviceMinor,
+	expectedErr string,
+) testDeviceMinorCache {
+	tc.Helper()
+	err := tc.Initialize(byRVName)
+	if !errIsExpected(err, expectedErr) {
+		tc.Fatalf("expected Initialize error to be %s, got %v", expectedErr, err)
+	}
+	return tc
+}
+
+func (tc testDeviceMinorCache) expect(
+	expectedLen int,
+	expectedMax DeviceMinor,
+	expectedReleased []DeviceMinor,
+) testDeviceMinorCache {
+	tc.Helper()
+	return tc.expectLen(expectedLen).expectMax(expectedMax).expectReleased(expectedReleased...)
+}
+
+func (tc testDeviceMinorCache) expectLen(expectedLen int) testDeviceMinorCache {
+	tc.Helper()
+	actualLen := tc.Len()
+	if expectedLen != actualLen {
+		tc.Fatalf("expected Len() to return %d, got %d", expectedLen, actualLen)
+	}
+	return tc
+}
+
+func (tc testDeviceMinorCache) expectMax(expectedMax DeviceMinor) testDeviceMinorCache {
+	tc.Helper()
+	actualMax := tc.Max()
+	if expectedMax != actualMax {
+		tc.Fatalf("expected Max() to return %d, got %d", expectedMax, actualMax)
+	}
+	return tc
+}
+
+func (tc testDeviceMinorCache) expectReleased(expectedReleased ...DeviceMinor) testDeviceMinorCache {
+	tc.Helper()
+	actualReleased := tc.Released()
+	if !slices.Equal(expectedReleased, actualReleased) {
+		tc.Fatalf("expected Released() to return %v, got %v", expectedReleased, actualReleased)
+	}
+	return tc
+}
+
+func (tc testDeviceMinorCache) cleanup() testDeviceMinorCache {
+	tc.Helper()
+	return tc.initialize(nil, "").expectEmpty()
+}
+
+func (tc testDeviceMinorCache) expectEmpty() testDeviceMinorCache {
+	tc.Helper()
+	return tc.expectLen(0).expectMax(0).expectReleased()
+}
+
+func errIsExpected(err error, expectedErr string) bool {
+	return ((err == nil) == (expectedErr == "")) && (err == nil || err.Error() == expectedErr)
+}
+
+func holes(d ...DeviceMinor) []DeviceMinor {
+	return d
+}
