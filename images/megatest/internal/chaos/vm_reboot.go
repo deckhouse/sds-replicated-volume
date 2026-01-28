3ed5af255ec0ef020d52ea8c09bfada989eae1a5
Ivan Ogurchenok <ivan.ogurchenok@flant.com>
2026-01-28 16:54:33 +0300

    [megatest] Add chaos network and VM management runners (#516)
    Signed-off-by: Ivan Ogurchenok <ivan.ogurchenok@flant.com>
    Signed-off-by: Pavel Karpov <pavel.karpov@flant.com>
    Co-authored-by: Pavel Karpov <pavel.karpov@flant.com>

---
+++ images/megatest/internal/chaos/vm_reboot.go
@@ -0,0 +1,162 @@
+/*
+Copyright 2025 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package chaos
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"time"
+
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// VirtualMachineOperation GVR
+var vmOpGVR = schema.GroupVersionResource{
+	Group:    "virtualization.deckhouse.io",
+	Version:  "v1alpha2",
+	Resource: "virtualmachineoperations",
+}
+
+// VMRebootManager manages VirtualMachineOperation for VM reboot chaos scenarios
+type VMRebootManager struct {
+	parentClient *ParentClient
+}
+
+// NewVMRebootManager creates a new VMRebootManager
+func NewVMRebootManager(parentClient *ParentClient) *VMRebootManager {
+	return &VMRebootManager{
+		parentClient: parentClient,
+	}
+}
+
+// CreateVMOperation creates a VirtualMachineOperation to control VM state
+func (m *VMRebootManager) CreateVMOperation(ctx context.Context, vmName string, opType VMOperationType, force bool) error {
+	namespace := m.parentClient.VMNamespace()
+	// Convert opType to lowercase for RFC 1123 subdomain compliance
+	opTypeLower := strings.ToLower(string(opType))
+	opName := fmt.Sprintf("chaos-%s-%s-%d", opTypeLower, vmName, time.Now().Unix())
+
+	vmOp := &unstructured.Unstructured{
+		Object: map[string]interface{}{
+			"apiVersion": "virtualization.deckhouse.io/v1alpha2",
+			"kind":       "VirtualMachineOperation",
+			"metadata": map[string]interface{}{
+				"name":      opName,
+				"namespace": namespace,
+				"labels": map[string]interface{}{
+					LabelChaosType:  string(ChaosTypeVMReboot),
+					LabelChaosNodeA: vmName,
+				},
+			},
+			"spec": map[string]interface{}{
+				"virtualMachineName": vmName,
+				"type":               string(opType),
+				"force":              force,
+			},
+		},
+	}
+
+	if err := m.parentClient.Client().Create(ctx, vmOp); err != nil {
+		return fmt.Errorf("creating VMOperation %s for VM %s: %w", opName, vmName, err)
+	}
+
+	return nil
+}
+
+// ListVMOperationsForVM returns all VirtualMachineOperations for a specific VM
+func (m *VMRebootManager) ListVMOperationsForVM(ctx context.Context, vmName string) ([]unstructured.Unstructured, error) {
+	namespace := m.parentClient.VMNamespace()
+	vmOpList := &unstructured.UnstructuredList{}
+	vmOpList.SetGroupVersionKind(schema.GroupVersionKind{
+		Group:   vmOpGVR.Group,
+		Version: vmOpGVR.Version,
+		Kind:    "VirtualMachineOperationList",
+	})
+
+	if err := m.parentClient.Client().List(ctx, vmOpList, client.InNamespace(namespace), client.MatchingLabels{
+		LabelChaosType:  string(ChaosTypeVMReboot),
+		LabelChaosNodeA: vmName,
+	}); err != nil {
+		return nil, fmt.Errorf("listing VMOperations for VM %s: %w", vmName, err)
+	}
+
+	return vmOpList.Items, nil
+}
+
+// HasUnfinishedVMOperations checks if there are unfinished VirtualMachineOperations for a VM
+// Returns true if there are operations with status.phase != Failed && != Completed
+func (m *VMRebootManager) HasUnfinishedVMOperations(ctx context.Context, vmName string) (bool, error) {
+	vmOps, err := m.ListVMOperationsForVM(ctx, vmName)
+	if err != nil {
+		return false, err
+	}
+
+	if len(vmOps) == 0 {
+		return false, nil
+	}
+
+	// Count operations with phase = Failed or Completed
+	finishedCount := 0
+	for _, vmOp := range vmOps {
+		phase, found, err := unstructured.NestedString(vmOp.Object, "status", "phase")
+		if err != nil {
+			// If we can't read phase, assume it's unfinished
+			return true, nil
+		}
+		if found && (phase == "Failed" || phase == "Completed") {
+			finishedCount++
+		}
+	}
+
+	// If count of finished operations != total count, there are unfinished operations
+	return finishedCount != len(vmOps), nil
+}
+
+// CleanupStaleVMOperations cleans up any leftover VMOperations from previous runs
+// Should be called at startup. Returns number of deleted operations.
+func (m *VMRebootManager) CleanupStaleVMOperations(ctx context.Context) (int, error) {
+	return m.cleanupVMOperationsByLabel(ctx)
+}
+
+func (m *VMRebootManager) cleanupVMOperationsByLabel(ctx context.Context) (int, error) {
+	namespace := m.parentClient.VMNamespace()
+	vmOpList := &unstructured.UnstructuredList{}
+	vmOpList.SetGroupVersionKind(schema.GroupVersionKind{
+		Group:   vmOpGVR.Group,
+		Version: vmOpGVR.Version,
+		Kind:    "VirtualMachineOperationList",
+	})
+
+	if err := m.parentClient.Client().List(ctx, vmOpList, client.InNamespace(namespace), client.MatchingLabels{
+		LabelChaosType: string(ChaosTypeVMReboot),
+	}); err != nil {
+		return 0, fmt.Errorf("listing VMOperations: %w", err)
+	}
+
+	deleted := 0
+	for _, vmOp := range vmOpList.Items {
+		if err := m.parentClient.Client().Delete(ctx, &vmOp); err == nil {
+			deleted++
+		}
+		// Ignore errors, best effort cleanup
+	}
+
+	return deleted, nil
+}
---
+++ images/megatest/internal/runners/chaos_network_blocker.go
@@ -0,0 +1,268 @@
+/*
+Copyright 2025 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package runners
+
+import (
+	"context"
+	"log/slog"
+	"math/rand"
+	"time"
+
+	"github.com/deckhouse/sds-replicated-volume/images/megatest/internal/chaos"
+	"github.com/deckhouse/sds-replicated-volume/images/megatest/internal/config"
+)
+
+const (
+	blockingEverythingProbability = 45
+	blockingDRBDProbability       = 90
+)
+
+// ChaosNetworkBlocker periodically blocks network between nodes with different incident types
+type ChaosNetworkBlocker struct {
+	cfg              config.ChaosNetworkBlockerConfig
+	networkBlockMgr  *chaos.NetworkBlockManager
+	parentClient     *chaos.ParentClient
+	log              *slog.Logger
+	forceCleanupChan <-chan struct{}
+}
+
+// NewChaosNetworkBlocker creates a new ChaosNetworkBlocker
+func NewChaosNetworkBlocker(
+	cfg config.ChaosNetworkBlockerConfig,
+	networkBlockMgr *chaos.NetworkBlockManager,
+	parentClient *chaos.ParentClient,
+	forceCleanupChan <-chan struct{},
+) *ChaosNetworkBlocker {
+	return &ChaosNetworkBlocker{
+		cfg:              cfg,
+		networkBlockMgr:  networkBlockMgr,
+		parentClient:     parentClient,
+		forceCleanupChan: forceCleanupChan,
+		log:              slog.Default().With("runner", "chaos-network-blocker"),
+	}
+}
+
+// Run starts the network blocking cycle until context is cancelled
+func (c *ChaosNetworkBlocker) Run(ctx context.Context) error {
+	c.log.Info("started")
+	defer c.log.Info("finished")
+
+	for {
+		// Wait random duration before next incident
+		if err := waitRandomWithContext(ctx, c.cfg.Period); err != nil {
+			return err
+		}
+
+		// Perform blocking
+		if err := c.doBlock(ctx); err != nil {
+			// Don't log error if context was cancelled (normal shutdown)
+			if ctx.Err() == context.Canceled {
+				return err
+			}
+			c.log.Error("blocking failed", "error", err)
+		}
+	}
+}
+
+func (c *ChaosNetworkBlocker) doBlock(ctx context.Context) error {
+	// Get list of nodes from parent cluster
+	nodes, err := c.parentClient.ListVMs(ctx)
+	if err != nil {
+		return err
+	}
+
+	if len(nodes) < 2 {
+		c.log.Debug("not enough nodes for network blocking", "node_count", len(nodes))
+		return nil
+	}
+
+	// Shuffle nodes randomly
+	//nolint:gosec // G404: math/rand is fine for non-security-critical random selection
+	rand.Shuffle(len(nodes), func(i, j int) {
+		nodes[i], nodes[j] = nodes[j], nodes[i]
+	})
+
+	// Determine incident duration
+	incidentDuration := randomDuration(c.cfg.IncidentDuration)
+
+	// Select incident type based on probability
+	//nolint:gosec // G404: math/rand is fine for non-security-critical random selection
+	randValue := rand.Intn(100)
+
+	switch {
+	case randValue < blockingEverythingProbability:
+		return c.doBlockingEverything(ctx, nodes, incidentDuration)
+	case randValue < blockingDRBDProbability:
+		return c.doBlockingDRBD(ctx, nodes, incidentDuration)
+	default:
+		return c.doSplitBrain(ctx, nodes, incidentDuration)
+	}
+}
+
+// doBlockingEverything blocks all TCP traffic between two random nodes
+func (c *ChaosNetworkBlocker) doBlockingEverything(ctx context.Context, nodes []chaos.NodeInfo, incidentDuration time.Duration) error {
+	nodeA, nodeB := nodes[0], nodes[1]
+
+	log := c.log.With(
+		"incident_type", "blocking-everything",
+		"node_a", nodeA.Name,
+		"node_b", nodeB.Name,
+		"duration", incidentDuration.String(),
+	)
+
+	log.Info("blocking all network")
+
+	policyName, err := c.networkBlockMgr.BlockAllNetwork(ctx, nodeA, nodeB)
+	if err != nil {
+		return err
+	}
+
+	select {
+	case <-ctx.Done():
+		// Cleanup on context cancellation
+		c.cleanup([]string{policyName})
+		return ctx.Err()
+	case <-c.forceCleanupChan:
+		// Cleanup on force signal
+		c.cleanup([]string{policyName})
+		return nil
+	case <-waitChan(incidentDuration):
+		// Normal timeout, unblock
+	}
+
+	log.Info("unblocking network")
+
+	cleanupCtx, cleanupCancel := context.WithTimeout(context.Background(), CleanupTimeout)
+	defer cleanupCancel()
+	if err := c.networkBlockMgr.UnblockTraffic(cleanupCtx, policyName); err != nil {
+		log.Error("failed to unblock network", "error", err)
+	}
+
+	return nil
+}
+
+// doBlockingDRBD is a stub for blocking-drbd incident (implementation deferred)
+func (c *ChaosNetworkBlocker) doBlockingDRBD(ctx context.Context, nodes []chaos.NodeInfo, incidentDuration time.Duration) error {
+	nodeA, nodeB := nodes[0], nodes[1]
+
+	_ = ctx
+
+	log := c.log.With(
+		"incident_type", "blocking-drbd",
+		"node_a", nodeA.Name,
+		"node_b", nodeB.Name,
+		"duration", incidentDuration.String(),
+	)
+
+	log.Info("blocking-drbd incident selected (not implemented yet)")
+
+	return nil
+}
+
+// doSplitBrain creates a network partition by splitting nodes into two groups
+func (c *ChaosNetworkBlocker) doSplitBrain(ctx context.Context, nodes []chaos.NodeInfo, incidentDuration time.Duration) error {
+	// Split nodes into two groups
+	groupA, groupB := c.splitNodes(nodes)
+
+	log := c.log.With(
+		"incident_type", "split-brain",
+		"group_a_size", len(groupA),
+		"group_b_size", len(groupB),
+		"group_a_nodes", nodeNames(groupA),
+		"group_b_nodes", nodeNames(groupB),
+		"duration", incidentDuration.String(),
+	)
+
+	log.Info("creating network partition")
+
+	// Create blocking policies between groups
+	var policyNames []string
+	for _, nodeA := range groupA {
+		for _, nodeB := range groupB {
+			policyName, err := c.networkBlockMgr.BlockAllNetwork(ctx, nodeA, nodeB)
+			if err != nil {
+				log.Error("failed to block network", "node_a", nodeA.Name, "node_b", nodeB.Name, "error", err)
+				continue
+			}
+			policyNames = append(policyNames, policyName)
+		}
+	}
+
+	log.Info("network partition created", "policy_count", len(policyNames))
+
+	select {
+	case <-ctx.Done():
+		// Cleanup on context cancellation
+		c.cleanup(policyNames)
+		return ctx.Err()
+	case <-c.forceCleanupChan:
+		// Cleanup on force signal
+		c.cleanup(policyNames)
+		return nil
+	case <-waitChan(incidentDuration):
+		// Normal timeout, remove partition
+	}
+
+	// Remove partition
+	log.Info("removing network partition", "policy_count", len(policyNames))
+	c.cleanup(policyNames)
+
+	return nil
+}
+
+func (c *ChaosNetworkBlocker) cleanup(policyNames []string) {
+	c.log.Debug("started cleanup")
+	defer c.log.Debug("finished cleanup")
+
+	cleanupCtx, cleanupCancel := context.WithTimeout(context.Background(), CleanupTimeout)
+	defer cleanupCancel()
+
+	for _, policyName := range policyNames {
+		if err := c.networkBlockMgr.UnblockTraffic(cleanupCtx, policyName); err != nil {
+			c.log.Error("cleanup failed", "policy", policyName, "error", err)
+		}
+	}
+}
+
+func (c *ChaosNetworkBlocker) splitNodes(nodes []chaos.NodeInfo) ([]chaos.NodeInfo, []chaos.NodeInfo) {
+	// Split into two groups
+	// If GroupSize is specified, use it as target size for group A
+	// Otherwise, split roughly in half
+	splitPoint := len(nodes) / 2
+	if c.cfg.GroupSize > 0 && c.cfg.GroupSize < len(nodes) {
+		splitPoint = c.cfg.GroupSize
+	}
+
+	// Ensure at least 1 node in each group
+	if splitPoint < 1 {
+		splitPoint = 1
+	}
+	if splitPoint >= len(nodes) {
+		splitPoint = len(nodes) - 1
+	}
+
+	return nodes[:splitPoint], nodes[splitPoint:]
+}
+
+// nodeNames extracts names from NodeInfo slice for logging
+func nodeNames(nodes []chaos.NodeInfo) []string {
+	names := make([]string, len(nodes))
+	for i, node := range nodes {
+		names[i] = node.Name
+	}
+	return names
+}
