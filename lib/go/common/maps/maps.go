b0790ff30dfaaae598e68b0d7901056c1c3371f8
Aleksandr Stefurishin <aleksandr.stefurishin@flant.com>
2026-02-12 17:06:31 +0300

    Fix linter errors
    Signed-off-by: Aleksandr Stefurishin <aleksandr.stefurishin@flant.com>

--- lib/go/common/maps/maps.go
+++ lib/go/common/maps/maps.go
@@ -21,8 +21,9 @@ import (
 	"iter"
 	"maps"
 
-	uiter "github.com/deckhouse/sds-common-lib/utils/iter"
 	"golang.org/x/exp/constraints"
+
+	uiter "github.com/deckhouse/sds-common-lib/utils/iter"
 )
 
 func SetUnique[K comparable, V any](m map[K]V, key K, value V) (map[K]V, bool) {
