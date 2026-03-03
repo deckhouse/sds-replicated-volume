3513ea8ed727c21dc594cbbef9be5bd1b0e520be
Slava V <vyacheslav.voytenok@flant.com>
2026-03-03 19:41:43 +0700

    Refactor error packages for consistency and clarity
    - Renamed packages from 'agenterrors' and 'ctlerrors' to 'errors' for uniformity across the codebase.
    - Updated import statements in validation tests to reflect the new package name, ensuring proper function calls.
    - Enhanced code readability and maintainability by standardizing error handling utilities.
    
    Signed-off-by: Slava V <vyacheslav.voytenok@flant.com>

--- images/controller/internal/ctlerrors/validation_test.go
+++ images/controller/internal/ctlerrors/validation_test.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package ctlerrors_test
+package errors_test
 
 import (
 	"testing"
@@ -26,7 +26,7 @@ import (
 func TestValidateArgNotNil(t *testing.T) {
 	var err error
 
-	err = ctlerrors.ValidateArgNotNil(nil, "testArgName")
+	err = errors.ValidateArgNotNil(nil, "testArgName")
 	if err == nil {
 		t.Fatal("ValidateArgNotNil() succeeded unexpectedly")
 	}
@@ -34,14 +34,14 @@ func TestValidateArgNotNil(t *testing.T) {
 	timeArg := time.Now()
 	timeArgPtr := &timeArg
 
-	err = ctlerrors.ValidateArgNotNil(timeArgPtr, "timeArgPtr")
+	err = errors.ValidateArgNotNil(timeArgPtr, "timeArgPtr")
 	if err != nil {
 		t.Fatalf("ValidateArgNotNil() failed: %v", err)
 	}
 
 	timeArgPtr = nil
 
-	err = ctlerrors.ValidateArgNotNil(timeArgPtr, "testArgName")
+	err = errors.ValidateArgNotNil(timeArgPtr, "testArgName")
 	if err == nil {
 		t.Fatal("ValidateArgNotNil() succeeded unexpectedly")
 	}
