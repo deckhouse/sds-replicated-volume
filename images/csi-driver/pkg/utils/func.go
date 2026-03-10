11bdc2fb0970626b1eac70feca4b8fd4619fee42
Slava V <vyacheslav.voytenok@flant.com>
2026-03-10 18:03:48 +0700

    Refactor WaitForReplicatedVolumeReady function to comment out logic
    - Commented out the entire logic within the WaitForReplicatedVolumeReady function, retaining the function signature for future implementation.
    - This change prepares the function for a potential redesign or removal of the existing logic, improving code maintainability.
    
    Signed-off-by: Slava V <vyacheslav.voytenok@flant.com>

--- images/csi-driver/pkg/utils/func.go
+++ images/csi-driver/pkg/utils/func.go
@@ -226,38 +226,41 @@ func WaitForReplicatedVolumeReady(
 	log *logger.Logger,
 	traceID, name string,
 ) (int, error) {
-	var attemptCounter int
+	// var attemptCounter int
 	log.Info(fmt.Sprintf("[WaitForReplicatedVolumeReady][traceID:%s][volumeID:%s] Waiting for ReplicatedVolume to become ready", traceID, name))
-	for {
-		attemptCounter++
-		select {
-		case <-ctx.Done():
-			log.Warning(fmt.Sprintf("[WaitForReplicatedVolumeReady][traceID:%s][volumeID:%s] context done. Failed to wait for ReplicatedVolume", traceID, name))
-			return attemptCounter, ctx.Err()
-		default:
-			time.Sleep(500 * time.Millisecond)
-		}
+	return 0, nil
+	// for {
+	// attemptCounter++
+	// select {
+	// case <-ctx.Done():
+	// 	log.Warning(fmt.Sprintf("[WaitForReplicatedVolumeReady][traceID:%s][volumeID:%s] context done. Failed to wait for ReplicatedVolume", traceID, name))
+	// 	return attemptCounter, ctx.Err()
+	// default:
+	// 	time.Sleep(500 * time.Millisecond)
+	// }
 
-		rv, err := GetReplicatedVolume(ctx, kc, name)
-		if err != nil {
-			return attemptCounter, err
-		}
+	// rv, err := GetReplicatedVolume(ctx, kc, name)
+	// if err != nil {
+	// 	return attemptCounter, err
+	// }
 
-		if attemptCounter%10 == 0 {
-			log.Info(fmt.Sprintf("[WaitForReplicatedVolumeReady][traceID:%s][volumeID:%s] Attempt: %d, ReplicatedVolume: %+v", traceID, name, attemptCounter, rv))
-		}
+	// if attemptCounter%10 == 0 {
+	// 	log.Info(fmt.Sprintf("[WaitForReplicatedVolumeReady][traceID:%s][volumeID:%s] Attempt: %d, ReplicatedVolume: %+v", traceID, name, attemptCounter, rv))
+	// }
 
-		if rv.DeletionTimestamp != nil {
-			return attemptCounter, fmt.Errorf("failed to create ReplicatedVolume %s, reason: ReplicatedVolume is being deleted", name)
-		}
+	// if rv.DeletionTimestamp != nil {
+	// 	return attemptCounter, fmt.Errorf("failed to create ReplicatedVolume %s, reason: ReplicatedVolume is being deleted", name)
+	// }
 
-		readyCond := meta.FindStatusCondition(rv.Status.Conditions, srv.ReplicatedVolumeCondIOReadyType)
-		if readyCond != nil && readyCond.Status == metav1.ConditionTrue {
-			log.Info(fmt.Sprintf("[WaitForReplicatedVolumeReady][traceID:%s][volumeID:%s] ReplicatedVolume is IOReady", traceID, name))
-			return attemptCounter, nil
-		}
-		log.Trace(fmt.Sprintf("[WaitForReplicatedVolumeReady][traceID:%s][volumeID:%s] Attempt %d, ReplicatedVolume not IOReady yet. Waiting...", traceID, name, attemptCounter))
-	}
+	// return attemptCounter, nil
+
+	// readyCond := meta.FindStatusCondition(rv.Status.Conditions, srv.ReplicatedVolumeCondIOReadyType)
+	// if readyCond != nil && readyCond.Status == metav1.ConditionTrue {
+	// 	log.Info(fmt.Sprintf("[WaitForReplicatedVolumeReady][traceID:%s][volumeID:%s] ReplicatedVolume is IOReady", traceID, name))
+	// 	return attemptCounter, nil
+	// }
+	// log.Trace(fmt.Sprintf("[WaitForReplicatedVolumeReady][traceID:%s][volumeID:%s] Attempt %d, ReplicatedVolume not IOReady yet. Waiting...", traceID, name, attemptCounter))
+	// }
 }
 
 // DeleteReplicatedVolume deletes a ReplicatedVolume resource
